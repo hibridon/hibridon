hibridon/hibridon

commit 24ca8943b8cb43da0a12c4cb52938f68b127efc3

removed open function's buffered argument when compiling with gfortran
buffered argument is unknown to gfortran's open() function

src/hiiolib_f.f
@@ -812,9 +812,15 @@ c     Stream i/o mode, replace, read/write
          if (openfl) then
             print *, '*** WARNING:  ', filnam, ' NOT CLOSED'
          end if
+cstart .not. unix-gfortran
+c;         open (unit=iunit, file=filnam, access='STREAM',
+c;     $        status='REPLACE', err=999, iostat=ierr,
+c;     $        buffered='YES')
+cend
+cstart unix-gfortran
          open (unit=iunit, file=filnam, access='STREAM',
-     $        status='REPLACE', err=999, iostat=ierr,
-     $        buffered='YES')
+     $        status='REPLACE', err=999, iostat=ierr)
+cend
          return
       end if
 c
@@ -828,9 +834,14 @@ c     Stream i/o mode, exist file, read/write
          if (openfl) then
             print *, '*** WARNING: ', filnam, 'NOT CLOSED'
          end if
-         open (unit=iunit, file=filnam, access='STREAM',
-     $        status='OLD', err=999, iostat=ierr,
-     $        buffered='YES')
+cstart .not. unix-gfortran
+c;         open (unit=iunit, file=filnam, access='STREAM',
+c;     $        status='OLD', err=999, iostat=ierr,
+c;     $        buffered='YES')
+cend
+cstart unix-gfortran
+         open (unit=iunit, file=filnam, status='OLD', err=999, iostat=ierr)
+cend
          return
       end if
 c

commit 40e0647a3d8bac096c2debbe1205c684183e831d

fixed bug that caused the s-matrix files to fail to load
These files were opened with the wrong access type (it should be 'STREAM')
This bug was accidentally introduced in commit 24ca8943b8

fixes #5

src/hiiolib_f.f
@@ -840,7 +840,7 @@ c;     $        status='OLD', err=999, iostat=ierr,
 c;     $        buffered='YES')
 cend
 cstart unix-gfortran
-         open (unit=iunit, file=filnam, status='OLD', err=999, iostat=ierr)
+         open (unit=iunit, file=filnam, access='STREAM', status='OLD', err=999, iostat=ierr)
 cend
          return
       end if