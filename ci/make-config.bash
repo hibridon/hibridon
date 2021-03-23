1d0e8845e6ab05b2feb6a4949028ace5e6be776d
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-03-23 18:30:13 +0100

    changes so that 'make config' works on debian in addition to ubuntu
    On physix-frontal.ipr.univ-rennes1.fr (debian stretch), libblas.a is located in /usr/lib, not /usr/lib/x86_64-linux-gnu. On debian or ubuntu systems, the make config code no longer checks for blas locations as the linker knows where the system libraries are.

--- ci/make-config.bash
+++ ci/make-config.bash
@@ -40,21 +40,37 @@ function unattended_makefirst()
         return $RETURNCODE_FAILURE
     fi
 
-    local la_lib_path='/usr/lib/x86_64-linux-gnu/'
+    local check_la_lib_location='true'
+    local la_lib_path='/usr/lib'
+    local os_name=$(lsb_release --short --id)
+    case $os_name in
+        'Debian'|'Ubuntu')
+            # On debian or ubuntu, the default libblas.a (which could point on blas, atlas or openblas, depending on update-alternatives) is sometimes in '/usr/lib', sometimes in '/usr/lib/x86_64-linux-gnu/' but the linker knows its location, as explained in https://askubuntu.com/questions/52617/what-is-usr-lib-i386-linux-gnu-for:
+            # > This change was made to enable installing versions of the same library compiled for different architectures (e.g. on an AMD64 system, one version might go in /usr/lib/x86_64-linux-gnu while the other goes in i386-linux-gnu).
+            # > Both the standard linker and dynamic linker know about these directories, so the change should be invisible for most applications. If the application is searching for actual library files manually, then it will need modification.
+            # > Details of the changes to Ubuntu can be found here:
+            # > https://wiki.ubuntu.com/MultiarchSpec
+            check_la_lib_location='false'
+            ;;
+        *)
+    esac
     local blas_lib_name='blas'
     local lapack_lib_name='lapack'
     local lib_name=''
 
-    # check that the library dev files are here
-    for lib_name in $blas_lib_name $lapack_lib_name
-    do
-        local lib_path="${la_lib_path}/lib${lib_name}.a"
-        if [ ! -f "$lib_path" ]
-        then
-            error "failed to find $lib_path"
-            return $RETURNCODE_FAILURE
-        fi
-    done
+    if [ "$check_la_lib_location" = 'true' ]
+    then
+        # check that the library dev files are here
+        for lib_name in $blas_lib_name $lapack_lib_name
+        do
+            local lib_path="${la_lib_path}/lib${lib_name}.a"
+            if [ ! -f "$lib_path" ]
+            then
+                error "failed to find $lib_path"
+                return $RETURNCODE_FAILURE
+            fi
+        done
+    fi
 
 	expect <<-EOF
 	spawn $hibridon_root_path/bin/makefirst
@@ -71,7 +87,7 @@ function unattended_makefirst()
 	expect -ex "Are you using Intel x86 (686 386) Architecture with PGI Lapack? \[y/n\]  "
 	send "n\r"
 	expect -ex "Enter the path for your BLAS library  "
-	send "$la_lib_path\r"
+	send "$la_lib_path/\r"
 	expect -ex "Enter the name  s of your libraries (-lxxx, -lyyy, etc, including the hypen)  "
 	send -- "-l$lapack_lib_name -l$blas_lib_name\r"
 	expect eof;
