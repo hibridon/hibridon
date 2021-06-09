6c732162de764ed97aeeb16ceee64b6781d377bd
Benjamin Desrousseaux <benjamin.desrousseaux@univ-rennes1.fr>
2021-06-10 00:21:55 +0200

    Add support for bound calculations

--- ci/comp_tests.f90
+++ ci/comp_tests.f90
@@ -4,9 +4,11 @@
   
 program comp_tests
     implicit none
-    character(len=200) :: ref, test
-    integer :: nstateso, nstates, it, i, j
+    character(len=200) :: ref, test, line
+    character(len=10) :: ext
+    integer :: nstateso, nstates, it, i, j, ios
     real(8), dimension(:,:,:), allocatable :: xs
+    real(8) :: dumr
 
     call get_command_argument(1,ref)
     call get_command_argument(2,test)
@@ -14,8 +16,39 @@ program comp_tests
     open(unit=1, status='old', file=trim(ref))
     open(unit=2, status='old', file=trim(test))
 
+    ! Extract file extension in ext string variable
+    j = scan(trim(ref),".", BACK= .true.)
+    ext = ref(j+1:len(ref))
 
-! READ REF AND TEST ICS
+if(ext=="evl") then 
+    j=0
+! READ REF AND TEST BOUND OUTPUT FILE
+    ! First get the number of evls
+        do 
+            read(1,'(A)',iostat=ios) line 
+            if(ios/=0) exit
+            if(index(line,'** EIGENVALUES (CM-1)') /=0 ) then
+                do; read(1,*,iostat=ios) dumr ; if(ios/=0) exit ; j=j+1 ; enddo
+            endif
+        enddo
+
+    ! Allocate and initialize xs
+    allocate(xs(2,j,1))
+    rewind(1)
+    ! Now read evls values from ref and test files
+    do it = 1, 2
+        do 
+            read(it,'(A)',iostat=ios) line 
+            if(ios/=0) exit
+            if(index(line,'** EIGENVALUES (CM-1)') /=0 ) then
+                do i = 1, j ; read(it,*) xs(it,i,1) ; write(*,*) xs(it,i,1) ; enddo
+            endif
+        enddo
+    enddo
+
+
+elseif(ext=="ics") then
+! READ REF AND TEST ICSOUTPUT FILE
 do it = 1, 2
     do i = 1, 6 ; read(it,*) ; enddo ! Skip the first 6 lines
     read(it,*) nstates, nstateso ! Read the total number of states and open channels
@@ -30,13 +63,16 @@ do it = 1, 2
         read(it,*) xs(it,i,:)
     enddo
 enddo
+endif
+
 close(1)
 close(2)
 
+
 ! COMPARE XS
 
-    do i=1,nstateso
-    do j=1,nstateso
+    do i=1,size(xs,2)
+    do j=1,size(xs,3)
         ! stop with code 2 if difference is more than 1% for one of the XSs.
         if( 100d0 * abs(xs(2,i,j) - xs(1,i,j))/max(xs(1,i,j),1d-300) > 1d0 ) then
          stop 1
