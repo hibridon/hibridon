fddcf18788b4c5897366852f2629499a2f9f4b63
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-06-10 18:59:37 +0200

    added hib_hegn_dgels test
    as part of issue #8, even if this test is not functional yet (see issue #1), and therefore disabled.

---
+++ tests/hib_hecn_dgels.cmake
@@ -0,0 +1,44 @@
+#------------------------------------------------------------------------------
+# hecn_dgels test
+#------------------------------------------------------------------------------
+
+add_executable(hib_hecn_dgels src/pot/pot_hecn_dgels.F src/himain.F)
+target_compile_definitions(hib_hecn_dgels PUBLIC KMAX=81)
+target_compile_definitions(hib_hecn_dgels PUBLIC T_MATRIX_SIZE=kmax)
+target_link_libraries(hib_hecn_dgels hib ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
+target_include_directories(hib_hecn_dgels PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src)
+target_include_directories(hib_hecn_dgels PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/src)
+target_compile_options(hib_hecn_dgels PRIVATE ${HIBRIDON_COMPILE_OPTIONS})
+
+
+add_test(NAME hib_hecn_dgels_test_setup
+    COMMAND bash -c "mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/tests/hib_hecn_dgels \
+                    &&  cp ${CMAKE_CURRENT_SOURCE_DIR}/tests/hecnx_hyp.com\
+                        ${CMAKE_CURRENT_SOURCE_DIR}/tests/Hecnx.inp \
+                        ${CMAKE_CURRENT_BINARY_DIR}/tests/hib_hecn_dgels"
+)
+
+add_test(NAME hib_hecn_dgels_build
+    COMMAND "${CMAKE_COMMAND}" --build ${CMAKE_BINARY_DIR} --target hib_hecn_dgels)
+
+add_test(NAME hib_hecn_dgels_test_run
+    COMMAND bash -c "cat ./hecnx_hyp.com | ${CMAKE_CURRENT_BINARY_DIR}/hib_hecn_dgels"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/tests/hib_hecn_dgels"
+)
+
+# Check that outputs from test runs are the same as the ref ones
+add_test(NAME hib_hecn_dgels_test_check
+    COMMAND bash -c "false"  # todo: not implemented yet (waiting for exe to work to see what files this test produces)
+    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+)
+
+add_test(NAME hib_hecn_dgels_test_cleanup
+    COMMAND rm -R ${CMAKE_CURRENT_BINARY_DIR}/tests/hib_hecn_dgels;
+)
+
+set_tests_properties(hib_hecn_dgels_test_setup PROPERTIES FIXTURES_SETUP hib_hecn_dgels_resources)
+set_tests_properties(hib_hecn_dgels_test_cleanup PROPERTIES FIXTURES_CLEANUP hib_hecn_dgels_resources)
+
+set_tests_properties(hib_hecn_dgels_test_run PROPERTIES FIXTURES_REQUIRED hib_hecn_dgels_resources)
+set_tests_properties(hib_hecn_dgels_test_run PROPERTIES DEPENDS hib_hecn_dgels_build)
+set_tests_properties(hib_hecn_dgels_test_check PROPERTIES DEPENDS check_outputs_build)
