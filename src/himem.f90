b3ecab3ff3ebdaa1f592c8ac257ead9b9cef010f
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-06-21 13:54:58 +0200

    converted common block cosc10 into module
    for issue #7

--- src/himem.f90
+++ src/himem.f90
@@ -646,15 +646,15 @@ end module mod_cofact
 !    end subroutine allocate_cosc9
 ! end module mod_cosc9
 
-! module mod_cosc10
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc10
-!    contains
-!    subroutine allocate_cosc10(n)
-!       integer, intent(in) :: n
-!       allocate(sc10(n))
-!    end subroutine allocate_cosc10
-! end module mod_cosc10
+module mod_cosc10
+   implicit none
+   real(8), dimension(:), allocatable :: sc10
+   contains
+   subroutine allocate_cosc10(amax)
+      integer, intent(in) :: amax
+      allocate(sc10(amax))
+   end subroutine allocate_cosc10
+end module mod_cosc10
 
 module mod_cosc11
    implicit none
