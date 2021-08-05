769298335c6e79a02d10da9ca4be2dd30bae2bcc
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-08-05 18:13:08 +0200

    added h2ohe test after checking that it passes
    work related to issue #19

---
+++ tests/h2ohe/h2ohe.cmake
@@ -0,0 +1,15 @@
+#------------------------------------------------------------------------------
+# h2ohe test
+# H2O--He SAPT Potential
+#------------------------------------------------------------------------------
+
+set(TEST_ID h2ohe)
+set(TEST_POT_SRC_FILE "pot_h2ohe.F")
+set(TEST_POT_DATA_FILES "h2o_coefd.dat h2o_params.dat h2o_coefi.dat")
+set(TEST_COMMAND_FILE "h2ohe_test.com")
+set(TEST_INPUT_FILES "H2ohe_test.inp")
+set(TEST_OUTPUT_FILES "H2ohe1.pcs H2ohe1.ics")
+set(TEST_KMAX 200)
+set(TEST_T_MATRIX_SIZE kmax)
+
+add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
