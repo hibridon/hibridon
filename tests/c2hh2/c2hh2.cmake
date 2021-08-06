2d7532f3cce5c018d5c7640a3b3ddb02617440b3
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-08-06 16:36:50 +0200

    added c2hh2 test after checking that it passes
    work related to issue #19

---
+++ tests/c2hh2/c2hh2.cmake
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# C2H - oH2
+#------------------------------------------------------------------------------
+
+set(TEST_ID c2hh2)
+set(TEST_POT_SRC_FILE "pot_c2hh2_12_6.F")
+set(TEST_POT_DATA_FILES "pot_c2hh2_12_6.dat")
+set(TEST_COMMAND_FILE "test_c2hh2.com")
+set(TEST_INPUT_FILES "C2hoh2.inp")
+set(TEST_OUTPUT_FILES "Job1.xxsc")
+set(TEST_KMAX 500)
+set(TEST_T_MATRIX_SIZE kmax)
+
+add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
