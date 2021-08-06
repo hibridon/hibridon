a6a391f93a1d78c52ce673339cdc66f833643392
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-08-06 11:00:48 +0200

    added ohh2_j13 test after checking that it passes
    -  I had to modify the reference xxsc file because I beleive that its format has changed since 2013 (the results are the same though)
    
    work related to issue #19

---
+++ tests/ohh2_j13/ohh2_j13.cmake
@@ -0,0 +1,15 @@
+#------------------------------------------------------------------------------
+# ohh2_j13 test
+# OH--H2 j2=1,3 built-in basis
+#------------------------------------------------------------------------------
+
+set(TEST_ID ohh2_j13)
+set(TEST_POT_SRC_FILE "pot_ohh2.F")
+set(TEST_POT_DATA_FILES "pot_ohh2_ccsdf12_avtzbf.dat")
+set(TEST_COMMAND_FILE "test_ohh2.com")
+set(TEST_INPUT_FILES "Ohh2.inp")
+set(TEST_OUTPUT_FILES "Job1.ics Job1.pcs Job1.xxsc")
+set(TEST_KMAX 1000)
+set(TEST_T_MATRIX_SIZE kmax)
+
+add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
