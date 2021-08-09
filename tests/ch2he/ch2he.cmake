c21711edd20070994d32d24ec4c5a039e2bda80a
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-08-09 10:58:06 +0200

    just comments

--- tests/ch2he/ch2he.cmake
+++ tests/ch2he/ch2he.cmake
@@ -24,6 +24,8 @@ set(TEST_COMMAND_FILE "ch2xhe_stmixtst.com")
 set(TEST_INPUT_FILES "Ch2x3he_para.inp")
 set(TEST_OUTPUT_FILES "Ch2x_p1.xsc Ch2x_p1.ics Ch2x_p1.hfx")
 #set(TEST_OUTPUT_FILES "Ch2x_p1.xsc Ch2x_p1.ics Ch2x_p1.hfx ch2he_x.stdout") # stdout files can't be automatically compared at the moment
+# todo: make sure that this test works : not sure it actually works because ch2he_x.stdout shows a message about a missing Ch2_p1.smt file... ? And ch2he_x.stdout is missing the end part
+
 set(TEST_KMAX 1451)
 set(TEST_T_MATRIX_SIZE kmax)
 
@@ -32,6 +34,8 @@ add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "
 #------------------------------------------------------------------------------
 # CH2(X,a) singlet-triplet mixing
 #------------------------------------------------------------------------------
+
+# disabled because ch2he_stmix.stdout simply says something about a missing Ch2_p1.smt file... ?
 
 # set(TEST_ID ch2he_stmix)
 # set(TEST_POT_SRC_FILE "pot_ch2he_x52_c20_v3.F")
