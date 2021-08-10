738af0b120595427168c10d2f093874f001fadf9
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-08-10 10:28:08 +0200

    made test ch2he_stmix work
    work related to issue #19

--- tests/ch2he/ch2he.cmake
+++ tests/ch2he/ch2he.cmake
@@ -21,7 +21,7 @@ set(TEST_ID ch2he_x)
 set(TEST_POT_SRC_FILE "pot_ch2he_x52_c20_v3.F")
 set(TEST_POT_DATA_FILES "ch2he_x52_v3.dat")
 set(TEST_COMMAND_FILE "ch2xhe_stmixtst.com")
-set(TEST_INPUT_FILES "Ch2x3he_para.inp Ch2_p1.smt")
+set(TEST_INPUT_FILES "Ch2x3he_para.inp Ch2_p1.smt") # Ch2_p1.smt is an output of ch2he_a test
 set(TEST_OUTPUT_FILES "Ch2x_p1.xsc Ch2x_p1.ics Ch2x_p1.hfx ch2he_x.stdout")
 
 set(TEST_KMAX 1451)
@@ -33,15 +33,13 @@ add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "
 # CH2(X,a) singlet-triplet mixing
 #------------------------------------------------------------------------------
 
-# disabled because ch2he_stmix.stdout simply says something about a missing Ch2_p1.smt file... ?
+set(TEST_ID ch2he_stmix)
+set(TEST_POT_SRC_FILE "pot_ch2he_x52_c20_v3.F")
+set(TEST_POT_DATA_FILES "ch2he_x52_v3.dat")
+set(TEST_COMMAND_FILE "ch2xhe_stmixtst2.com")
+set(TEST_INPUT_FILES "Ch2x3he_para.inp Ch2x_p1.smt") # Ch2x_p1.smt is an output of ch2he_x test
+set(TEST_OUTPUT_FILES "ch2he_stmix.stdout")
+set(TEST_KMAX 1451)
+set(TEST_T_MATRIX_SIZE kmax)
 
-# set(TEST_ID ch2he_stmix)
-# set(TEST_POT_SRC_FILE "pot_ch2he_x52_c20_v3.F")
-# set(TEST_POT_DATA_FILES "ch2he_x52_v3.dat")
-# set(TEST_COMMAND_FILE "ch2xhe_stmixtst2.com")
-# set(TEST_INPUT_FILES "Ch2x3he_para.inp")
-# set(TEST_OUTPUT_FILES "ch2he_stmix.stdout")
-# set(TEST_KMAX 1451)
-# set(TEST_T_MATRIX_SIZE kmax)
-
-# add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
+add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
