ec98e1c537789300f76f4cbe051421af2ce741ad
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-08-12 11:16:54 +0200

    added ch3he_vib_quick test
    a quick version of ch3he_vib test for code coverage (6s instead of 282s)
    
    work related to issue #25

--- tests/ch3he_vib/ch3he_vib.cmake
+++ tests/ch3he_vib/ch3he_vib.cmake
@@ -13,3 +13,19 @@ set(TEST_KMAX 1000)
 set(TEST_T_MATRIX_SIZE kmax)
 
 add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
+
+#------------------------------------------------------------------------------
+# CH3(X)--He Vibrational Relaxation (quick version for code coverage)
+# 4D CCSD(T) PES, user defined pot/basis routine (ch3he_vib) by Q. Ma
+#------------------------------------------------------------------------------
+
+set(TEST_ID ch3he_vib_quick)
+set(TEST_POT_SRC_FILE "pot_ch3he_vib.F")
+set(TEST_POT_DATA_FILES "pot_ch3he_vib_data pot_ch3he_vib_ylmasym pot_ch3he_vib_ylmsym")
+set(TEST_COMMAND_FILE "ch3he_vib_quick.com")
+set(TEST_INPUT_FILES "Ch3he_vib_quick.inp")
+set(TEST_OUTPUT_FILES "Vibq1.ics Vibq1.xxsc")
+set(TEST_KMAX 1000)
+set(TEST_T_MATRIX_SIZE kmax)
+
+add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
---
+++ tests/ch3he_vib/ch3he_vib_quick.com
@@ -0,0 +1,4 @@
+inp=ch3he_vib_quick.inp
+job=vibq
+run
+intcrs
