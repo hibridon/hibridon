47337be16ac6eb44c770a3ee8489c7be71ebbfbe
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-08-12 09:56:20 +0200

    added ch2he_x_quick test
    a quick version of ch2he_x test for code coverage
    
    work related to issue #25

--- tests/ch2he/ch2he.cmake
+++ tests/ch2he/ch2he.cmake
@@ -30,6 +30,22 @@ set(TEST_T_MATRIX_SIZE kmax)
 add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
 
 #------------------------------------------------------------------------------
+# CH2(X)--He and spin-resolved cross sections (quick version for test coverage)
+#------------------------------------------------------------------------------
+
+set(TEST_ID ch2he_x_quick)
+set(TEST_POT_SRC_FILE "pot_ch2he_x52_c20_v3.F")
+set(TEST_POT_DATA_FILES "ch2he_x52_v3.dat")
+set(TEST_COMMAND_FILE "ch2xhe_stmixtst_quick.com")
+set(TEST_INPUT_FILES "Ch2x3he_para.inp Ch2_p1.smt") # Ch2_p1.smt is an output of ch2he_a test
+set(TEST_OUTPUT_FILES "Ch2x_pq1.xsc Ch2x_pq1.ics Ch2x_pq1.hfx ch2he_x_quick.stdout")
+
+set(TEST_KMAX 1451)
+set(TEST_T_MATRIX_SIZE kmax)
+
+add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_KMAX}" "${TEST_T_MATRIX_SIZE}")
+
+#------------------------------------------------------------------------------
 # CH2(X,a) singlet-triplet mixing
 #------------------------------------------------------------------------------
 
