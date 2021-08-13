hibridon/hibridon

commit 7327475b8a3f4421924fcaf6c777b093a5a1a90a

converted common blocks cojhld and coehld into modules
for issue #7

src/hibrid1.F
@@ -857,6 +857,8 @@ c#if (defined(HIB_UNIX) || defined(HIB_MAC)) && !defined(HIB_UNIX_IBM)
 *   current revision date 19-mar-1996
 *  ------------------------------------------------------------
       use mod_codim, only: mmax
+      use mod_cojhld, only: jout1 => jhold ! jout1(1)
+      use mod_coehld, only: jout2 => ehold ! jout2(1)
       use mod_coinhl, only: jlev => inhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       use mod_coisc2, only: ipack1 => isc2 ! ipack1(1)
@@ -882,8 +884,6 @@ c#if (defined(HIB_UNIX) || defined(HIB_MAC)) && !defined(HIB_UNIX_IBM)
       common /cojq/ jq(1)
       common /colq/ lq(1)
       common /coinq/ inq(1)
-      common /cojhld/ jout1(1)
-      common /coehld/ jout2(1)
 c
       zero=0
       acc=0

src/himem.f90
@@ -396,25 +396,25 @@ end module mod_cofact
 !    end subroutine allocate_coinq
 ! end module mod_coinq
 
-! module mod_cojhld
-!    implicit none
-!    integer, dimension(:), allocatable :: jhold
-!    contains
-!    subroutine allocate_cojhld(n)
-!       integer, intent(in) :: n
-!       allocate(jhold(n))
-!    end subroutine allocate_cojhld
-! end module mod_cojhld
+module mod_cojhld
+   implicit none
+   integer, dimension(:), allocatable :: jhold
+   contains
+   subroutine allocate_cojhld(n)
+      integer, intent(in) :: n
+      allocate(jhold(n))
+   end subroutine allocate_cojhld
+end module mod_cojhld
 
-! module mod_coehld
-!    implicit none
-!    real(8), dimension(:), allocatable :: ehold
-!    contains
-!    subroutine allocate_coehld(n)
-!       integer, intent(in) :: n
-!       allocate(ehold(n))
-!    end subroutine allocate_coehld
-! end module mod_coehld
+module mod_coehld
+   implicit none
+   real(8), dimension(:), allocatable :: ehold
+   contains
+   subroutine allocate_coehld(n)
+      integer, intent(in) :: n
+      allocate(ehold(n))
+   end subroutine allocate_coehld
+end module mod_coehld
 
 module mod_coinhl
    implicit none

src/hibrid2.F
@@ -927,6 +927,7 @@ c
 *  ------------------------------------------------------------------
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
+      use mod_cojhld, only: jlev => jhold ! jlev(5)
       use mod_coisc1, only: inlev => isc1 ! inlev(5)
       use mod_coisc2, only: jpoint => isc2 ! jpoint(5)
       use mod_cosc1, only: elev => sc1 ! elev(5)
@@ -946,11 +947,10 @@ c
       integer i, ienerg, iout, isize, j, jbegin, jend, jfinal,
      :        jfirst, jj1, jj2, jlpar, jtemp, jtotd, lenx, n, nlevel,
      :        nlevop, nout, numax, numin, nud, iaver
-      integer ipoint, jlev
+      integer ipoint
 #include "common/parpot"
       common /coz/ zmat(1)
       common /cow/ scmat(1)
-      common /cojhld/ jlev(5)
       common /colq/ ipoint(5)
       common /coselb/ ibasty
       dimension  a(3)
@@ -1234,14 +1234,14 @@ caber
       return
       end
       subroutine aver1 (zmat, scmat, n)
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       implicit double precision (a-h,o-z)
 *  subroutine to sum and average cross section matrix over positive
 *  and negative values of index
-      integer i, ind, index, j, jnd, n, nn, jlev
+      integer i, ind, index, j, jnd, n, nn
 *     real scmat, zmat
       dimension zmat(1), scmat(n,n)
-      common /cojhld/ jlev(1)
 *  first copy cross section matrix into scmat
       call matmov (zmat, scmat, n, n, n, n)
       nn = n / 2
@@ -1268,6 +1268,7 @@ caber
 * -------------------------------------------------
       subroutine xscpr1 (zmat, nlevop, isize, iaver,
      :                   ipos, iprint, flaghf)
+      use mod_cojhld, only: jlev => jhold ! jlev(4)
       use mod_cosc1, only: elev => sc1 ! elev(4)
       use mod_coisc1, only: inlev => isc1 ! inlev(4)
 
@@ -1277,11 +1278,10 @@ caber
 *  if iaver = 1, then nth and (n+1) st rows are added before printing
       integer i, isize, iskip, j, jcol, jhigh, jj, jlow, jmax,
      :        jrow, ncol, nlevop, iaver
-      integer ind, ipoint, jlev
+      integer ind, ipoint
 *     real elev, zmat
       logical ipos, iprint, flaghf
       dimension zmat(nlevop,nlevop), ind(50)
-      common /cojhld/ jlev(4)
       common /colq/ ipoint(4)
       common /coselb/ ibasty
 *     first transpose the cross section matrix so that initial
@@ -1434,6 +1434,7 @@ caber
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
       use constants
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
 cmha
       use mod_coisc2, only: jpoint => isc2 ! jpoint(5)
@@ -1457,7 +1458,6 @@ cmha
       common /coz/ zmat(1)
       common /cow/ scmat(1)
       common /coamat/ zbuf(1)
-      common /cojhld/ jlev(1)
       common /colq/ ipoint(1)
       common /coener/ energ(1)
       common /coselb/ ibasty
@@ -1814,15 +1814,15 @@ cmha
       return
       end
       subroutine aver2 (zmat, scmat, n)
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
 
       implicit double precision (a-h,o-z)
 *  subroutine to sum and average cross section matrix over positive
 *  and negative values of index
-      integer i, ind, index, j, jnd, n, nn, jlev
+      integer i, ind, index, j, jnd, n, nn
 *     real scmat, zmat
       dimension zmat(1), scmat(n,n)
-      common /cojhld/ jlev(1)
 *  first copy cross section matrix into scmat
       call matmov (zmat, scmat, n, n, n, n)
       nn = n / 2
@@ -1849,6 +1849,7 @@ cmha
 * ------------------------------------------------------
       subroutine xscpr2 (zmat, xthresh, nlevop, isize, iaver,
      :                   iprint,isa)
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       use mod_cosc1, only: elev => sc1 ! elev(1)
       implicit double precision (a-h,o-z)
@@ -1858,13 +1859,12 @@ cmha
 *  if iaver = 1, then nth and (n+1) st rows are added before printing
       integer i, isize, iskip, j, jcol, jhigh, jj, jlow, jmax,
      :        jrow, ncol, nlevop, iaver
-      integer ind, ipoint, jlev, isa
+      integer ind, ipoint, isa
       logical airyfl, airypr, bastst, batch, chlist, csflag, flaghf,
      :                flagsu, ihomo, ipos, iprint,lpar(17)
       dimension zmat(nlevop,nlevop), ind(50)
       common /colpar/ airyfl, airypr, bastst, batch, chlist,
      :                csflag, flaghf, flagsu, ihomo, ipos,lpar
-      common /cojhld/ jlev(1)
       common /colq/ ipoint(1)
 *  first transpose cross section matrix so that initial states are
 *  columns and final states are rows

src/hibrid5.F
@@ -1105,6 +1105,7 @@ cABER
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
       use constants
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       use mod_coisc2, only: nj, jlist => isc2 ! nj,jlist(1)
       use mod_cosc1, only: elev => sc1 ! elev(1)
@@ -1117,7 +1118,6 @@ cABER
       character*80 line
       logical csflag, flaghf, iprint, flagsu, twomol, existf, nucros
 #include "common/parpot"
-      common /cojhld/ jlev(1)
       common /coselb/ ibasty
       dimension  a(4),scmat(nmax,1)
 

src/hidifcrs.F
@@ -37,6 +37,7 @@
       use mod_codim, only: mmax
       use mod_coj12, only: j12
       use mod_coj12p, only: j12pk
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       use mod_coisc2, only: jout1 => isc2 ! jout1(1)
       use mod_cosc1, only: elev => sc1 ! elev(1)
@@ -62,7 +63,6 @@
       common /cojq/ jq(1)
       common /colq/ lq(1)
       common /coinq/ inq(1)
-      common /cojhld/ jlev(1)
       real(8), dimension(:), allocatable :: s, sm, sm6
 * to store a22p,a21p and a11p amplitudes
       complex(8), dimension(:, :), allocatable :: fm1m2

src/himain.F
@@ -10,6 +10,8 @@
       use mod_covvl, only: allocate_covvl
       use mod_cofact, only: allocate_cofact
 
+      use mod_cojhld, only: allocate_cojhld, jhold
+      use mod_coehld, only: allocate_coehld, ehold
       use mod_coinhl, only: allocate_coinhl, inhold
       use mod_coisc1, only: allocate_coisc1, isc1
       use mod_coisc2, only: allocate_coisc2, isc2
@@ -171,8 +173,8 @@
       common /cojq/ jq(kmax)
       common /colq/ lq(kmax)
       common /coinq/ inq(kmax)
-      common /cojhld/ jhold(kmax)
-      common /coehld/ ehold(kmax)
+      call allocate_cojhld(kmax)
+      call allocate_coehld(kmax)
       call allocate_coinhl(kmax)
       call allocate_coisc1(kmax)
       call allocate_coisc2(kmax)

src/hitensor.F
@@ -2305,6 +2305,7 @@ c jlpar=-1:  skip backward to index for jtotp, jlpar=1
 *------------------------------------------------------------------------
       use mod_codim, only: mmax
       use mod_cov2, only: nv2max, ndummy, y => v2
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
 * common blocks for levels for which xs's to be computed
       use mod_coisc9, only: jslist => isc9 ! jslist(1)
@@ -2329,7 +2330,6 @@ c jlpar=-1:  skip backward to index for jtotp, jlpar=1
       common /colq/ lq(1)
       common /coinq/ inq(1)
       common /coisc2 / jout1(1)
-      common /cojhld/ jlev(1)
       common /coz/ sreal(1)
       common /cow/ simag(1)
       common /coconv/ econv, xmconv, ang2c
@@ -2755,6 +2755,7 @@ c=============
 *------------------------------------------------------------------------
       use mod_codim, only: mmax
       use mod_cov2, only: nv2max, ndummy, y => v2
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
 * common blocks for levels for which xs's to be computed
       use mod_coisc9, only: jslist => isc9 ! jslist(1)
@@ -2779,7 +2780,6 @@ c=============
       common /colq/ lq(1)
       common /coinq/ inq(1)
       common /coisc2 / jout1(1)
-      common /cojhld/ jlev(1)
       common /coz/ sreal(1)
       common /cow/ simag(1)
       common /coconv/ econv, xmconv, ang2c

src_reformat/hibrid2_jk.f
@@ -928,6 +928,7 @@ cjk          form='(1x,i3,i4,i7,  (t18,10(1pd11.3)))'
 *  ------------------------------------------------------------------
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
+      use mod_cojhld, only: jlev => jhold ! jlev(5)
       use mod_coisc1, only: inlev => isc1 ! inlev(5)
       use mod_coisc2, only: jpoint => isc2 ! jpoint(5)
       use mod_cosc1, only: elev => sc1 ! elev(5)
@@ -951,7 +952,6 @@ cjk          form='(1x,i3,i4,i7,  (t18,10(1pd11.3)))'
       include "common/parpot"
       common /coz/ zmat(1)
       common /cow/ scmat(1)
-      common /cojhld/ jlev(5)
       common /colq/ ipoint(5)
       common /coselb/ ibasty
       dimension  a(3)
@@ -1232,6 +1232,7 @@ caber
       return
       end
       subroutine aver1 (zmat, scmat, n)
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       implicit double precision (a-h,o-z)
 *  subroutine to sum and average cross section matrix over positive
@@ -1239,7 +1240,6 @@ caber
       integer i, ind, index, j, jnd, n, nn, inlev, jlev
 *     real scmat, zmat
       dimension zmat(1), scmat(n,n)
-      common /cojhld/ jlev(1)
 *  first copy cross section matrix into scmat
       call matmov (zmat, scmat, n, n, n, n)
       nn = n / 2
@@ -1266,6 +1266,7 @@ caber
 * -------------------------------------------------
       subroutine xscpr1 (zmat, nlevop, isize, iaver,
      :                   ipos, iprint, flaghf)
+      use mod_cojhld, only: jlev => jhold ! jlev(4)
       use mod_coisc1, only: inlev => isc1 ! inlev(4)
       use mod_cosc1, only: elev => sc1 ! elev(4)
       implicit double precision (a-h,o-z)
@@ -1278,7 +1279,6 @@ caber
 *     real elev, zmat
       logical ipos, iprint, flaghf
       dimension zmat(nlevop,nlevop), ind(50)
-      common /cojhld/ jlev(4)
       common /colq/ ipoint(4)
       common /coselb/ ibasty
 *     first transpose the cross section matrix so that initial
@@ -1433,6 +1433,7 @@ cjk 70            format (f5.1, i5, 2x, 13 (1pe10.3,1x) )
       use constants
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
 cmha
       use mod_coisc2, only: jpoint => isc2 ! jpoint(5)
@@ -1456,7 +1457,6 @@ cmha
       common /coz/ zmat(1)
       common /cow/ scmat(1)
       common /coamat/ zbuf(1)
-      common /cojhld/ jlev(1)
       common /colq/ ipoint(1)
       common /coener/ energ(1)
       common /coselb/ ibasty
@@ -1810,6 +1810,7 @@ cmha
       return
       end
       subroutine aver2 (zmat, scmat, n)
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       implicit double precision (a-h,o-z)
 *  subroutine to sum and average cross section matrix over positive
@@ -1817,7 +1818,6 @@ cmha
       integer i, ind, index, j, jnd, n, nn, inlev, jlev
 *     real scmat, zmat
       dimension zmat(1), scmat(n,n)
-      common /cojhld/ jlev(1)
 *  first copy cross section matrix into scmat
       call matmov (zmat, scmat, n, n, n, n)
       nn = n / 2
@@ -1844,6 +1844,7 @@ cmha
 * ------------------------------------------------------
       subroutine xscpr2 (zmat, xthresh, nlevop, isize, iaver,
      :                   iprint,isa)
+      use mod_cojhld, only: jlev => jhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       use mod_cosc1, only: elev => sc1 ! elev(1)
       implicit double precision (a-h,o-z)
@@ -1859,7 +1860,6 @@ cmha
       dimension zmat(nlevop,nlevop), ind(50)
       common /colpar/ airyfl, airypr, bastst, batch, chlist,
      :                csflag, flaghf, flagsu, ihomo, ipos,lpar
-      common /cojhld/ jlev(1)
       common /colq/ ipoint(1)
 *  first transpose cross section matrix so that initial states are
 *  columns and final states are rows

commit 4b4ed780392bb73594229721714ec3ea21dbb00d

fixed harmless regression introduced in 7327475b8a3f4421924fcaf6c777b093a5a1a90a
in difs, jout2 was an array of reals instead of an array of integers

The fact that this change passes all tests indicates that eholdint is only used in difs and could therefore be removed from mod_coehld

src/hibrid1.F
@@ -861,7 +861,7 @@ c#if (defined(HIB_UNIX) || defined(HIB_MAC)) && !defined(HIB_UNIX_IBM)
       use mod_colq, only: lq ! lq(1)
       use mod_coinq, only: inq ! inq(1)
       use mod_cojhld, only: jout1 => jhold ! jout1(1)
-      use mod_coehld, only: jout2 => ehold ! jout2(1)
+      use mod_coehld, only: jout2 => eholdint ! jout2(1)
       use mod_coinhl, only: jlev => inhold ! jlev(1)
       use mod_coisc1, only: inlev => isc1 ! inlev(1)
       use mod_coisc2, only: ipack1 => isc2 ! ipack1(1)

src/himem.f90
@@ -420,10 +420,12 @@ end module mod_cojhld
 module mod_coehld
    implicit none
    real(8), dimension(:), allocatable :: ehold
+   integer, dimension(:), allocatable :: eholdint
    contains
    subroutine allocate_coehld(n)
       integer, intent(in) :: n
       allocate(ehold(n))
+      allocate(eholdint(n))
    end subroutine allocate_coehld
 end module mod_coehld
 