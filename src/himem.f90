hibridon/hibridon

commit 45f08404d5bd41e206da7314c260187f22be630a

converted common blocks cosc1-8 into modules
for issue #7

src/hibrid1.F
@@ -857,6 +857,11 @@ c#if (defined(HIB_UNIX) || defined(HIB_MAC)) && !defined(HIB_UNIX_IBM)
 *   current revision date 19-mar-1996
 *  ------------------------------------------------------------
       use mod_codim, only: mmax
+      use mod_cosc1, only: elev => sc1 ! elev(1)
+      use mod_cosc2, only: jpack1 => sc2 ! jpack1(1)
+      use mod_cosc3, only: jpack2 => sc3 ! jpack2(1)
+      use mod_cosc4, only: lpack1 => sc4 ! lpack1(1)
+      use mod_cosc5, only: lpack2 => sc5 ! lpack2(1)
       implicit double precision (a-h,o-z)
       character*(*) fname1,fname2
       character*20 cdate1,cdate2
@@ -877,11 +882,6 @@ c#if (defined(HIB_UNIX) || defined(HIB_MAC)) && !defined(HIB_UNIX_IBM)
       common /coehld/ jout2(1)
       common /coinhl/ jlev(1)
       common /coisc1/ inlev(1)
-      common /cosc1/ elev(1)
-      common /cosc2/ jpack1(1)
-      common /cosc3/ jpack2(1)
-      common /cosc4/ lpack1(1)
-      common /cosc5/ lpack2(1)
       common /coisc2/ ipack1(1)
       common /coisc3/ ipack2(1)
 c

src/hibrid5.F
@@ -1105,6 +1105,9 @@ cABER
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
       use constants
+      use mod_cosc1, only: elev => sc1 ! elev(1)
+      use mod_cosc2, only: csum => sc2 ! csum(1)
+      use mod_cosc3, only: tsum => sc3 ! tsum(1)
       implicit double precision (a-h,o-z)
       character*(*) fname
       character*20 cdate
@@ -1115,9 +1118,6 @@ cABER
       common /cojhld/ jlev(1)
       common /coisc1/ inlev(1)
       common /coisc2/ nj,jlist(1)
-      common /cosc1/ elev(1)
-      common /cosc2/ csum(1)
-      common /cosc3/ tsum(1)
       common /coselb/ ibasty
       dimension  a(4),scmat(nmax,1)
 
@@ -1391,6 +1391,9 @@ c
 * ------------------------------------------------------------------
       use mod_cosout
       use constants
+      use mod_cosc1, only: elev => sc1 ! elev(1)
+      use mod_cosc2, only: inlev => sc2 ! inlev(1)
+      use mod_cosc3, only: jlev => isc3 ! jlev(1)
       implicit double precision (a-h,o-z)
       character*(*) filnam
       character*40  icsfil, smtfil, xname
@@ -1418,9 +1421,6 @@ c
       common /coisc5/ inpack(1)
       common /coisc6/ isc1(1)
       common /coisc7/ isc2(1)
-      common /cosc1/  elev(1)
-      common /cosc2/  inlev(1)
-      common /cosc3/  jlev(1)
       common /coered/ ered, rmu
       common /codim/  nairy, mmax
       common /coselb/ ibasty
@@ -1611,6 +1611,9 @@ c
       use mod_cosout, only: nnout, jout
       use mod_coj12, only: j12
       use mod_coj12p, only: j12pk
+      use mod_cosc1, only: elev => sc1 ! elev(1)
+      use mod_cosc2, only: inlev => sc2 ! inlev(1)
+      use mod_cosc3, only: jlev => sc3 ! jlev(1)
       implicit double precision (a-h,o-z)
       logical twomol, flagsu, nucros
       logical csflag, flaghf, lpar1, lpar2, batch, ipos, lpar3
@@ -1622,9 +1625,6 @@ c
       common /coisc5/ inpack(1)
       common /coisc6/ isc1(1)
       common /coisc7/ isc2(1)
-      common /cosc1/  elev(1)
-      common /cosc2/  inlev(1)
-      common /cosc3/  jlev(1)
       common /coisc2/ nj,jlist(1)
       common /colpar/ lpar1(3), batch, lpar2(5), ipos, lpar3(17)
       common /coselb/ ibasty

src/hidifcrs.F
@@ -37,6 +37,10 @@
       use mod_codim, only: mmax
       use mod_coj12, only: j12
       use mod_coj12p, only: j12pk
+      use mod_cosc1, only: elev => sc1 ! elev(1)
+      use mod_cosc2, only: jpack1 => sc2 ! jpack1(1)
+      use mod_cosc3, only: lpack1 => sc3 ! lpack1(1)
+      use mod_cosc4, only: ipack1 => sc4 ! ipack1(1)
       implicit double precision (a-h,o-z)
       character*(*) fname1
       character*20 cdate1
@@ -59,10 +63,6 @@
       common /cojhld/ jlev(1)
       common /coisc1/ inlev(1)
       common /coisc2/ jout1(1)
-      common /cosc1/ elev(1)
-      common /cosc2/ jpack1(1)
-      common /cosc3/ lpack1(1)
-      common /cosc4/ ipack1(1)
       real(8), dimension(:), allocatable :: s, sm, sm6
 * to store a22p,a21p and a11p amplitudes
       complex(8), dimension(:, :), allocatable :: fm1m2

src/himem.f90
@@ -556,75 +556,77 @@ end module mod_cofact
 !    end subroutine allocate_colsc1
 ! end module mod_colsc1
 
-! module mod_cosc1
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc1
-!    contains
-!    subroutine allocate_cosc1(n)
-!       integer, intent(in) :: n
-!       allocate(sc1(n))
-!    end subroutine allocate_cosc1
-! end module mod_cosc1
+module mod_cosc1
+   implicit none
+   real(8), dimension(:), allocatable :: sc1
+   contains
+   subroutine allocate_cosc1(n)
+      integer, intent(in) :: n
+      allocate(sc1(n))
+   end subroutine allocate_cosc1
+end module mod_cosc1
 
-! module mod_cosc2
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc2
-!    contains
-!    subroutine allocate_cosc2(n)
-!       integer, intent(in) :: n
-!       allocate(sc2(n))
-!    end subroutine allocate_cosc2
-! end module mod_cosc2
+module mod_cosc2
+   implicit none
+   real(8), dimension(:), allocatable :: sc2
+   contains
+   subroutine allocate_cosc2(n)
+      integer, intent(in) :: n
+      allocate(sc2(n))
+   end subroutine allocate_cosc2
+end module mod_cosc2
 
-! module mod_cosc3
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc3
-!    contains
-!    subroutine allocate_cosc3(n)
-!       integer, intent(in) :: n
-!       allocate(sc3(n))
-!    end subroutine allocate_cosc3
-! end module mod_cosc3
+module mod_cosc3
+   implicit none
+   real(8), dimension(:), allocatable :: sc3
+   integer, dimension(:), allocatable :: isc3
+   contains
+   subroutine allocate_cosc3(n)
+      integer, intent(in) :: n
+      allocate(sc3(n))
+      allocate(isc3(n))
+   end subroutine allocate_cosc3
+end module mod_cosc3
 
-! module mod_cosc4
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc4
-!    contains
-!    subroutine allocate_cosc4(n)
-!       integer, intent(in) :: n
-!       allocate(sc4(n))
-!    end subroutine allocate_cosc4
-! end module mod_cosc4
+module mod_cosc4
+   implicit none
+   real(8), dimension(:), allocatable :: sc4
+   contains
+   subroutine allocate_cosc4(n)
+      integer, intent(in) :: n
+      allocate(sc4(n))
+   end subroutine allocate_cosc4
+end module mod_cosc4
 
-! module mod_cosc5
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc5
-!    contains
-!    subroutine allocate_cosc5(n)
-!       integer, intent(in) :: n
-!       allocate(sc5(n))
-!    end subroutine allocate_cosc5
-! end module mod_cosc5
+module mod_cosc5
+   implicit none
+   real(8), dimension(:), allocatable :: sc5
+   contains
+   subroutine allocate_cosc5(n)
+      integer, intent(in) :: n
+      allocate(sc5(n))
+   end subroutine allocate_cosc5
+end module mod_cosc5
 
-! module mod_cosc6
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc6
-!    contains
-!    subroutine allocate_cosc6(n)
-!       integer, intent(in) :: n
-!       allocate(sc6(n))
-!    end subroutine allocate_cosc6
-! end module mod_cosc6
+module mod_cosc6
+   implicit none
+   real(8), dimension(:), allocatable :: sc6
+   contains
+   subroutine allocate_cosc6(n)
+      integer, intent(in) :: n
+      allocate(sc6(n))
+   end subroutine allocate_cosc6
+end module mod_cosc6
 
-! module mod_cosc7
-!    implicit none
-!    real(8), dimension(:), allocatable :: sc7
-!    contains
-!    subroutine allocate_cosc7(n)
-!       integer, intent(in) :: n
-!       allocate(sc7(n))
-!    end subroutine allocate_cosc7
-! end module mod_cosc7
+module mod_cosc7
+   implicit none
+   real(8), dimension(:), allocatable :: sc7
+   contains
+   subroutine allocate_cosc7(n)
+      integer, intent(in) :: n
+      allocate(sc7(n))
+   end subroutine allocate_cosc7
+end module mod_cosc7
 
 module mod_cosc8
    implicit none

src/hibrid2.F
@@ -927,6 +927,7 @@ c
 *  ------------------------------------------------------------------
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
+      use mod_cosc1, only: elev => sc1 ! elev(5)
       implicit double precision (a-h,o-z)
       character*(*) fname
       character*20 cdate
@@ -951,7 +952,6 @@ c
       common /coisc1/ inlev(5)
       common /coisc2/ jpoint(5)
       common /colq/ ipoint(5)
-      common /cosc1/ elev(5)
       common /coselb/ ibasty
       dimension  a(3)
       data econv / 219474.6d0/
@@ -1268,6 +1268,8 @@ caber
 * -------------------------------------------------
       subroutine xscpr1 (zmat, nlevop, isize, iaver,
      :                   ipos, iprint, flaghf)
+      use mod_cosc1, only: elev => sc1 ! elev(4)
+
       implicit double precision (a-h,o-z)
 *      current revision date: 16-dec-2007
 *  subroutine to print out specified columns of cross section matrix
@@ -1281,7 +1283,6 @@ caber
       common /cojhld/ jlev(4)
       common /coisc1/ inlev(4)
       common /colq/ ipoint(4)
-      common /cosc1/ elev(4)
       common /coselb/ ibasty
 *     first transpose the cross section matrix so that initial
 *     states are columns and final states are rows
@@ -1433,6 +1434,7 @@ caber
       use mod_cosout, only: nnout, jout
       use mod_coiout, only: niout, indout
       use constants
+      use mod_cosc1, only: elev => sc1 ! elev(1)
       implicit double precision (a-h,o-z)
       character*(*) fname
       character*20 cdate
@@ -1457,7 +1459,6 @@ cmha
       common /coisc2/ jpoint(5)
 cmha
       common /colq/ ipoint(1)
-      common /cosc1/ elev(1)
       common /coener/ energ(1)
       common /coselb/ ibasty
       dimension  a(4)
@@ -1847,6 +1848,7 @@ cmha
 * ------------------------------------------------------
       subroutine xscpr2 (zmat, xthresh, nlevop, isize, iaver,
      :                   iprint,isa)
+      use mod_cosc1, only: elev => sc1 ! elev(1)
       implicit double precision (a-h,o-z)
 
 *  current revision date:  10-oct-2001 by ab
@@ -1863,7 +1865,6 @@ cmha
       common /cojhld/ jlev(1)
       common /coisc1/ inlev(1)
       common /colq/ ipoint(1)
-      common /cosc1/ elev(1)
 *  first transpose cross section matrix so that initial states are
 *  columns and final states are rows
       call transp (zmat, nlevop, nlevop)

src/hibrid4.F
@@ -33,6 +33,7 @@
       use mod_coj12, only: j12
       use mod_coj12p, only: j12pk
       use constants
+      use mod_cosc1, only: elev => sc1 ! elev(1)
       implicit double precision (a-h,o-z)
       character*(*) fname
       character*20 cdate
@@ -47,7 +48,6 @@
       common /colq/ lq(1)
       common /coinq/ inq(1)
       common /coinhl/ jlev(1)
-      common /cosc1/ elev(1)
       common /coisc2/ jpack(1)
       common /coisc3/ lpack(1)
       common /coisc1/ inlev(1)
@@ -1042,6 +1042,12 @@ c     current revision: 20-apr-2012 by q. ma
 c
 * -------------------------------------------------------
       use mod_coeint, only: eint
+      use mod_cosc1, only: sc1 ! sc1(10)
+      use mod_cosc2, only: sc2 ! sc2(10)
+      use mod_cosc3, only: sc3 ! sc3(10)
+      use mod_cosc4, only: sc4 ! sc4(10)
+      use mod_cosc5, only: sc5 ! sc5(10)
+
       implicit double precision (a-h,o-z)
       character*48 oldlab, oldpot
       character*20 cdate, olddat
@@ -1067,11 +1073,6 @@ c
       common /cotq1/  dpsir(25)
       common /cotq2/  dpsii(25)
       common /coisc1/ isc1(25)
-      common /cosc1/ sc1(10)
-      common /cosc2/ sc2(10)
-      common /cosc3/ sc3(10)
-      common /cosc4/ sc4(10)
-      common /cosc5/ sc5(10)
       common /cowave/ irec, ifil, nchwfu, ipos2, ipos3, nrlogd, iendwv,
      $     inflev
       dimension iword(32), word(4)
@@ -1346,6 +1347,14 @@ c
       use mod_coqvec, only: nphoto
       use mod_cocent, only: sc2 => cent
       use mod_coeint, only: eint
+      use mod_cosc1, only: pk  => sc1   ! pk(100)
+      use mod_cosc2, only: fj  => sc2   ! fj(10)
+      use mod_cosc3, only: fjp => sc3   ! fjp(10)
+      use mod_cosc4, only: fn  => sc4   ! fn(10)
+      use mod_cosc5, only: fnp => sc5   ! fnp(10)
+      use mod_cosc6, only: sc  => sc6   ! sc(100)
+      use mod_cosc7, only: sc1  => sc7   ! sc1(100)
+
       implicit double precision (a-h,o-z)
       character*(*) filnam
       character*40  psifil, wavfil, amplfil, flxfil
@@ -1374,13 +1383,6 @@ c
       common /coisc2/ nlist(50)
       common /coisc3/ nalist(60)
       common /coisc5/ nblist(60)
-      common /cosc1/ pk(100)
-      common /cosc2/ fj(10)
-      common /cosc3/ fjp(10)
-      common /cosc4/ fn(10)
-      common /cosc5/ fnp(10)
-      common /cosc6/ sc(100)
-      common /cosc7/ sc1(100)
 * common for y1, y2, y4
       common /coered/ ered, rmu
       common /codim/  nairy, mmax
@@ -2303,6 +2305,9 @@ c
 * ------------------------------------------------------------------
       use mod_coiout, only: niout, indout
       use mod_cocent, only: sc2 => cent
+      use mod_cosc1, only: pk => sc1 ! pk(6)
+      use mod_cosc6, only: sc => sc6 ! sc(60)
+      use mod_cosc7, only: sc1 => sc7 ! sc1(6)
       use mod_cosc8, only: sc8
       use mod_cosc9, only: sc9
       implicit double precision (a-h,o-z)
@@ -2319,9 +2324,6 @@ c
       common /cotq3/  scmat3(100)
       common /coisc2/ nlist(60)
       common /coisc3/ nalist(60)
-      common /cosc1/ pk(6)
-      common /cosc6/ sc(60)
-      common /cosc7/ sc1(6)
       common /coered/ ered, rmu
       common /coground/ ifull
       common /cotrans/ ttrans(36)
@@ -2618,6 +2620,8 @@ c
 *
 * ------------------------------------------------------------------
       use mod_cocent, only: sc2 => cent
+      use mod_cosc6, only: sc => sc6 ! sc(6)
+      use mod_cosc7, only: sc7  ! sc7(6)
       use mod_cosc8, only: sc8
       implicit double precision (a-h,o-z)
       common /coered/ ered, rmu
@@ -2625,8 +2629,6 @@ c
      $     inflev
       common /coz/    scmat(100)
       common /coisc3/ nalist(10)
-      common /cosc6/ sc(6)
-      common /cosc7/ sc7(6)
 * common for y1, y2, y4
       data zero, one, onemin, two,   conv
      :    /0.d0, 1.d0, -1.d0, 2.d0, 219474.6d0/
@@ -2691,14 +2693,14 @@ c
 * revised on 30-mar-2012 by q. ma for stream I/O of wfu files
 *
 * ------------------------------------------------------------------
+      use mod_cosc6, only: sc => sc6 ! sc(6)
+      use mod_cosc7, only: sc1 => sc7 ! sc1(6)
       implicit double precision (a-h,o-z)
       common /cowave/ irec, ifil, nchwfu, ipos2, ipos3, nrlogd, iendwv,
      $     inflev
       common /coamat/ psir(100)
       common /cow/    sr(100)
       common /cozmat/ si(100)
-      common /cosc6/ sc(6)
-      common /cosc7/ sc1(6)
       common /coisc2/ nlist(6)
         irec=npts+4
         do 180 kstep=1, npts
@@ -2733,6 +2735,8 @@ c
 * current revision: 20-apr-2012 by q. ma
 *
 * ------------------------------------------------------------------
+      use mod_cosc6, only: sc => sc6 ! sc(6)
+      use mod_cosc7, only: sc1 => sc7 ! sc1(6)
       implicit double precision (a-h,o-z)
       logical renormf
       common /cowave/ irec, ifil, nchwfu, ipos2, ipos3, nrlogd, iendwv,
@@ -2740,8 +2744,6 @@ c
       common /coamat/ psir(100)
       common /cow/    sr(100)
       common /cozmat/ si(100)
-      common /cosc6/ sc(6)
-      common /cosc7/ sc1(6)
       common /coisc2/ nlist(6)
       dimension scrvec(64)
       irec=npts+4

src/hihypxsc.F
@@ -29,6 +29,8 @@
 *     current revision:  8-jan-2018 by p. dagdigian
 *     -------------------------------------------------------------
       use mod_coj12, only: j12q => j12
+      use mod_cosc1, only: elev => sc1 ! elev(1)
+      use mod_cosc3, only: elevh => sc3 ! elevh(1)
       implicit double precision (a-h,o-z)
       complex(8) t, tf
       character*(*) flname
@@ -41,11 +43,9 @@
       common /coisc1/ jlev(1)
       common /coisc3/ inlev(1)
       common /coisc5/ jout(1)
-      common /cosc1/ elev(1)
       common /coisc7/ jlevh(1)
       common /coisc8/ iflevh(1)
       common /coisc9/ inlevh(1)
-      common /cosc3/ elevh(1)
       common /coisc10/ ipack(1)
       common /coisc11/ jpack(1)
       common /coisc12/ lpack(1)

src/hiiolib_c.F
@@ -285,8 +285,8 @@
       inquire (file=input, exist=existf)
       if (.not. existf) then
         write (6, 20) input(1:length)
-20      format
-     :   (/'   *** INPUT FILE ',(a),' DOES NOT EXIST; ABORT ***')
+20      format(
+     :   /'   *** INPUT FILE ',(a),' DOES NOT EXIST; ABORT ***')
         if( batch) call exit
         return
       end if
@@ -910,6 +910,8 @@
 *
 *  subroutines called: open
 *  --------------------------------------------------------------------
+      use mod_cosc1, only: rsc1 => sc1 ! rsc1(2)
+      use mod_cosc2, only: rsc2 => sc2 ! rsc2(1)
       implicit double precision (a-h,o-z)
       integer ifile, nerg, nfile, lenx, isize, isizes
       logical airyfl, airypr, bastst, batch, chlist, csflag, existf,
@@ -934,8 +936,6 @@
       common /coisc2/ isc2(1)
       common /coisc3/ isc3(3)
       common /coisc4/  isc4(1)
-      common /cosc1/  rsc1(2)
-      common /cosc2/  rsc2(1)
       common /colsc1/  lsc1(5)
       common /coselb/ ibasty
       if (nerg .gt. 1) then

commit 8e3d459ada0b46062081bf9de92afb5a4de30e42

fixed regressions introduced by commit 45f08404d5bd41e206da7314c260187f22be630a

src/hibrid1.F
@@ -858,10 +858,10 @@ c#if (defined(HIB_UNIX) || defined(HIB_MAC)) && !defined(HIB_UNIX_IBM)
 *  ------------------------------------------------------------
       use mod_codim, only: mmax
       use mod_cosc1, only: elev => sc1 ! elev(1)
-      use mod_cosc2, only: jpack1 => sc2 ! jpack1(1)
-      use mod_cosc3, only: jpack2 => sc3 ! jpack2(1)
-      use mod_cosc4, only: lpack1 => sc4 ! lpack1(1)
-      use mod_cosc5, only: lpack2 => sc5 ! lpack2(1)
+      use mod_cosc2, only: jpack1 => sc2int ! jpack1(1)
+      use mod_cosc3, only: jpack2 => sc3int ! jpack2(1)
+      use mod_cosc4, only: lpack1 => sc4int ! lpack1(1)
+      use mod_cosc5, only: lpack2 => sc5int ! lpack2(1)
       implicit double precision (a-h,o-z)
       character*(*) fname1,fname2
       character*20 cdate1,cdate2

src/hibrid5.F
@@ -1393,8 +1393,8 @@ c
       use mod_cosout
       use constants
       use mod_cosc1, only: elev => sc1 ! elev(1)
-      use mod_cosc2, only: inlev => sc2 ! inlev(1)
-      use mod_cosc3, only: jlev => isc3 ! jlev(1)
+      use mod_cosc2, only: inlev => sc2int ! inlev(1)
+      use mod_cosc3, only: jlev => sc3int ! jlev(1)
       implicit double precision (a-h,o-z)
       character*(*) filnam
       character*40  icsfil, smtfil, xname
@@ -1612,8 +1612,8 @@ c
       use mod_coj12, only: j12
       use mod_coj12p, only: j12pk
       use mod_cosc1, only: elev => sc1 ! elev(1)
-      use mod_cosc2, only: inlev => sc2 ! inlev(1)
-      use mod_cosc3, only: jlev => sc3 ! jlev(1)
+      use mod_cosc2, only: inlev => sc2int ! inlev(1)
+      use mod_cosc3, only: jlev => sc3int ! jlev(1)
       implicit double precision (a-h,o-z)
       logical twomol, flagsu, nucros
       logical csflag, flaghf, lpar1, lpar2, batch, ipos, lpar3

src/hidifcrs.F
@@ -37,9 +37,9 @@
       use mod_coj12, only: j12
       use mod_coj12p, only: j12pk
       use mod_cosc1, only: elev => sc1 ! elev(1)
-      use mod_cosc2, only: jpack1 => sc2 ! jpack1(1)
-      use mod_cosc3, only: lpack1 => sc3 ! lpack1(1)
-      use mod_cosc4, only: ipack1 => sc4 ! ipack1(1)
+      use mod_cosc2, only: jpack1 => sc2int ! jpack1(1)
+      use mod_cosc3, only: lpack1 => sc3int ! lpack1(1)
+      use mod_cosc4, only: ipack1 => sc4int ! ipack1(1)
       implicit double precision (a-h,o-z)
       character*(*) fname1
       character*20 cdate1

src/himem.f90
@@ -577,45 +577,59 @@ module mod_cosc1
    end subroutine allocate_cosc1
 end module mod_cosc1
 
+! general purpose array of scalars
+! note : sc2 and sc2int are never used at the same time, as they initially were 2 views of the same data
 module mod_cosc2
    implicit none
    real(8), dimension(:), allocatable :: sc2
+   integer, dimension(:), allocatable :: sc2int
    contains
    subroutine allocate_cosc2(n)
       integer, intent(in) :: n
       allocate(sc2(n))
+      allocate(sc2int(n))
    end subroutine allocate_cosc2
 end module mod_cosc2
 
+! general purpose array of scalars
+! note : sc3 and sc3int are never used at the same time, as they initially were 2 views of the same data
 module mod_cosc3
    implicit none
    real(8), dimension(:), allocatable :: sc3
-   integer, dimension(:), allocatable :: isc3
+   integer, dimension(:), allocatable :: sc3int
    contains
    subroutine allocate_cosc3(n)
       integer, intent(in) :: n
       allocate(sc3(n))
-      allocate(isc3(n))
+      allocate(sc3int(n))
    end subroutine allocate_cosc3
 end module mod_cosc3
 
+! general purpose array of scalars
+! note : sc4 and sc4int are never used at the same time, as they initially were 2 views of the same data
 module mod_cosc4
    implicit none
    real(8), dimension(:), allocatable :: sc4
+   integer, dimension(:), allocatable :: sc4int
    contains
    subroutine allocate_cosc4(n)
       integer, intent(in) :: n
       allocate(sc4(n))
+      allocate(sc4int(n))
    end subroutine allocate_cosc4
 end module mod_cosc4
 
+! general purpose array of scalars
+! note : sc5 and sc5int are never used at the same time, as they initially were 2 views of the same data
 module mod_cosc5
    implicit none
    real(8), dimension(:), allocatable :: sc5
+   integer, dimension(:), allocatable :: sc5int
    contains
    subroutine allocate_cosc5(n)
       integer, intent(in) :: n
       allocate(sc5(n))
+      allocate(sc5int(n))
    end subroutine allocate_cosc5
 end module mod_cosc5
 