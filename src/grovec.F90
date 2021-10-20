4090c3f9ffc9f720ef3d844c9cf217101988e6e4
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-10-20 17:49:41 +0200

    cleaned up grovec
    - moved grovec test into unittests dir
    - added documention on grovec type

--- src/grovec.F90
+++ src/grovec.F90
@@ -5,6 +5,8 @@
 #define CONCATENATE(a,b) IDENTITY(a)IDENTITY(b)
 #endif
 
+! grovec : GROwable VECtor
+! grovec provides a way to store a sparse array of size n in the form of blocks that are allocated when needed
 module mod_grovec
   implicit none
   private
@@ -36,27 +38,3 @@ contains
 #undef GROVEC_CLASS_NAME
 
 end module mod_grovec
-
-!#define TESTING_GROVEC
-#ifdef TESTING_GROVEC
-program test_grovec
-use mod_grovec, only: dgrovec_type, igrovec_type
-type(dgrovec_type) :: dg1
-type(igrovec_type) :: ig1
-dg1 = dgrovec_type(block_size=1024*1024, num_blocks=1024)
-call dg1%set_element(int(1, 8), 3.d0)
-call dg1%append(4.d0)
-call dg1%set_element(int(1024, 8), 1024.d0)
-call dg1%set_element(int(1024*1024+1, 8), 1025.d0)
-write (6,*) 'dg1(1)=', dg1%get_element(1)
-write (6,*) 'dg1(2)=', dg1%get_element(2)
-write (6,*) 'dg1(1024)=', dg1%get_element(1024)
-write (6,*) 'dg1(1024*1024+1)=', dg1%get_element(1024*1024+1)
-ig1 = igrovec_type(block_size=1024, num_blocks=16)
-call ig1%append(42)
-write (6,*) 'ig1(1)=', ig1%get_element(1)
-call ig1%empty()
-call ig1%append(666)
-write (6,*) 'ig1(1)=', ig1%get_element(1)
-end program test_grovec
-#endif
