d0ca56801d860b743d37ca81f67dd6c448ebebac
Benjamin Desrousseaux <benjamin.desrousseaux@univ-rennes1.fr>
2022-01-11 20:04:13 +0100

    First tests

--- cmake/modules/add_hibexe.cmake
+++ cmake/modules/add_hibexe.cmake
@@ -14,21 +14,67 @@ function(add_hibexe EXE_NAME POT_SRC_FILE p_T_MATRIX_SIZE)
   target_include_directories(${EXE_NAME} PRIVATE ${hibridon_BINARY_DIR}/lib) # to find the .mod files
 
 
-  # Compile options
-  target_compile_options(${EXE_NAME}
-    PUBLIC
-    $<$<CONFIG:DEBUG>:-Og>
-    $<$<CONFIG:DEBUG>:-fsanitize=address>         # Address sanitizer
-    $<$<BOOL:${ENABLE_CODE_COVERAGE}>:--coverage> # Code coverage (same as -fprofile-arcs -ftest-coverage at compile time)
-  )
+################################################################################
+#
+#    COMPILE OPTIONS
+#
+################################################################################
 
-  # Link options
-  target_link_options(${EXE_NAME}
-    PUBLIC
-    $<$<CONFIG:DEBUG>:-Og>
-    $<$<CONFIG:DEBUG>:-fsanitize=address>          # Address sanitizer
-    $<$<BOOL:${ENABLE_CODE_COVERAGE}>:--coverage>  # Code coverage (same as -lgcov at link time)
-  )
+# GNU (gfortran)
+if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+    target_compile_options(hib
+      PUBLIC
+      # Non-specific options
+      -std=legacy                               # Allow pre-Fortran 77 syntax (e.g. arbitrary length arrays)
+      -fno-automatic                            # Put everything on the heap
+      $<$<PLATFORM_ID:Linux>:-march=native>     # Tune generated code for host's micro-architecture
+      $<$<PLATFORM_ID:Darwin>:-mtune=native>    # Tune generated code for host's micro-architecture (march not working on Darwin)
+      $<$<PLATFORM_ID:Linux>:-mcmodel=large>    # Required on Linux
+      # Config-specific options: RELEASE
+      $<$<CONFIG:RELEASE>:-O3>                  # Optimization level at 3 for Release
+      $<$<CONFIG:RELEASE>:-finit-local-zero>    # Init variables to zero/false/null
+      # Config-specific options: DEBUG
+      $<$<CONFIG:DEBUG>:-Og>                    # Optimization level at 0 and generates complete debugging information
+      $<$<CONFIG:DEBUG>:-fbacktrace>            # Generates extra information in the object file to provide source file traceback information when a severe error occurs at run time
+      $<$<CONFIG:DEBUG>:-Wall>                  # Enable all warnings
+      $<$<CONFIG:DEBUG>:-Wextra>                # Enable extra warnings
+      $<$<CONFIG:DEBUG>:-fsanitize=address>     # Address sanitizer
+      $<$<CONFIG:DEBUG>:-Wuninitialized>        # Emit warnings for uninitialized variables 
+      $<$<BOOL:${ENABLE_CODE_COVERAGE}>:--coverage> # Code coverage (same as -fprofile-arcs -ftest-coverage at compile time)
+    )
+# Intel (ifort)
+elseif (Fortran_COMPILER_NAME STREQUAL "ifort")
+    target_compile_options(hib
+      PUBLIC
+      # Non-specific options
+      -heap-arrays                                # Put everything on the heap
+      -no-wrap-margin                             # Don't wrap output files
+      # Config-specific options: RELEASE
+      $<$<CONFIG:RELEASE>:-O3>                    # Optimization level at 3 for Release
+      $<$<CONFIG:RELEASE>:-xHost>                 # Generate instructions for the highest instruction set available on the compilation host processor
+      $<$<CONFIG:RELEASE>:-init=zero>             # Init variables to zero/false/null
+      # Config-specific options: DEBUG
+      $<$<CONFIG:DEBUG>:-Od>                      # Disable all optimizations
+      $<$<CONFIG:DEBUG>:-debug>                   # Generates complete debugging information
+      $<$<CONFIG:DEBUG>:-traceback>               # Generates extra information in the object file to provide source file traceback information when a severe error occurs at run time
+      $<$<CONFIG:DEBUG>:-warn all>                # Enable all warnings
+    )
+endif()
 
+################################################################################
+#
+#    LINK OPTIONS
+#
+################################################################################
+
+# GNU (gfortran)
+if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+    target_link_options(hib
+      PUBLIC
+      $<$<CONFIG:DEBUG>:-fsanitize=address>         # Address sanitizer
+      $<$<BOOL:${ENABLE_CODE_COVERAGE}>:--coverage> # Code coverage (same as -lgcov at link time)
+    )
+# Intel (ifort)
+endif()
 
 endfunction(add_hibexe)
