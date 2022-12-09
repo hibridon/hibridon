f77fbcce767042eab6a34b3a40af14aa0629649d
Benjamin Desrousseaux <benjamin.desrousseaux@gmx.fr>
2022-12-09 09:37:56 +0100

    Adavance in refactroing

---
+++ lib/hypxsc/hyp_levels.F90
@@ -0,0 +1,12 @@
+!*******************************************************************************
+!     This module contains all the procedures necessary to compute 
+!     hyperfine levels
+!*******************************************************************************
+module mod_hyp_levels
+    implicit none
+
+    public  :: 
+    
+    contains
+
+end module mod_hyp_levels
