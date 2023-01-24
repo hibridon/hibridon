6f4ce0f461f00ddde838e7db90340246d61a8a35
Benjamin Desrousseaux <benjamin.desrousseaux@gmx.fr>
2023-01-24 10:00:20 +0100

    Add BASTST test for basis 08

--- tests/BASTST/pot_bastst.F90
+++ tests/BASTST/pot_bastst.F90
@@ -69,6 +69,11 @@ integer, intent(in) :: ibasty
             lammin(1) = 0 
             lammax(1) = 10 
             mproj(1) = 0  
+        case (8)
+            ntv = 1
+            lammin(1) = 2
+            lammax(1) = 4
+            mproj(1) = 0
         case (28)
             nlam = 3
             allocate(lms_1sg1sg(nlam))
