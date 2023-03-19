6517370f4eeb22f83d1ec6323e3a94f8c664d2f1
Benjamin Desrousseaux <benjamin.desrousseaux@gmx.fr>
2023-03-19 05:14:33 +0100

    Add BASTST for basis 13

--- tests/BASTST/pot_bastst.F90
+++ tests/BASTST/pot_bastst.F90
@@ -91,6 +91,10 @@ integer, intent(in) :: ibasty
             lammin(1)=1
             lammax(1)=9
             mproj(1)=0
+        case (13)
+            lammin(1)=1
+            lammax(1)=9
+            mproj(1)=0
         case (14)
             lammin(1) = 1 ; lammin(2) = 2
             lammax(1) = 1 ; lammax(2) = 2
