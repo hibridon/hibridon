hibridon/hibridon

commit cdc62a68520c11378b348ea36dd0e7620b6c0910

fixed warnings related to unused ang2c
work related to #174

lib/histmix.F90
@@ -32,7 +32,7 @@ use mod_coisc9, only: inlevt => isc9 ! inlevt(1)
 use mod_cosc1, only: elev1 => sc1 ! elev1(1)
 use mod_cosc2, only: elev2 => sc2 ! elev2(1)
 use mod_cosc3, only: elevt => sc3 ! elevt(1)
-use constants, only: econv, xmconv, ang2 => ang2c
+use constants, only: econv, xmconv
 use mod_parpot, only: potnam=>pot_name, label=>pot_label
 use mod_hiutil, only: gennam, mtime, gettim
 use mod_hismat, only: sread, rdhead, sinqr
@@ -697,7 +697,7 @@ use mod_coisc9, only: inlevt => isc9 ! inlevt(1)
 use mod_cosc1, only: elev1 => sc1 ! elev1(1)
 use mod_cosc2, only: elev2 => sc2 ! elev2(1)
 use mod_cosc3, only: elevt => sc3 ! elevt(1)
-use constants, only: econv, xmconv, ang2 => ang2c
+use constants, only: econv, xmconv
 use mod_parpot, only: potnam=>pot_name, label=>pot_label
 use mod_hiutil, only: xf6j
 implicit double precision (a-h, o-z)

lib/bases/hiba06_stp.F90
@@ -139,7 +139,7 @@ use mod_conlam, only: nlam
 use mod_cosysi, only: isicod, ispar
 use mod_cosysr, only: isrcod, rspar
 use mod_hibasutil, only: vlmstp
-use constants, only: econv, xmconv, ang2c
+use constants, only: econv, xmconv
 use mod_par, only: iprint
 use mod_parbas, only: lammin, lammax, mproj
 use mod_par, only: boundc

lib/hibrid2.F90
@@ -25,7 +25,7 @@ use mod_cosys, only: scod
 use mod_cosysi, only: nscode, isicod
 use mod_cosysl, only: islcod
 use mod_cosysr, only: isrcod
-use constants, only: econv, xmconv, ang2c
+use constants, only: econv, xmconv
 use mod_par, only: airyfl, prairy, bastst, batch, chlist, &
                 csflag, flaghf, flagsu, ihomo, ipos, logdfl, &
                 prlogd, noprin, prpart, readpt, rsflag, prsmat, &

lib/hibrid5.F90
@@ -2224,26 +2224,37 @@ subroutine partcr (tsq,  scmat, nopen, &
 !     corrected degeneracy factor for ibasty = 23
 !
 ! ----------------------------------------------------------------------
-use constants
+use constants, only: ang2c, pi
 use mod_hibasis, only: is_j12
 use mod_selb, only: ibasty
 use mod_ered, only: ered, rmu
 use mod_hivector, only: dset
 use mod_hitypes, only: bqs_type
-implicit double precision (a-h,o-z)
+implicit none
 real(8), dimension(nmax,nmax), intent(in) :: tsq
-!      real(8), dimension(:,:), intent(in), target :: tototsq
 real(8), dimension(nmax,nmax), intent(out) :: scmat
+integer, intent(in) :: nopen
 type(bqs_type), intent(in) :: row_bqs
 type(bqs_type), intent(in) :: col_bqs
 integer, dimension(nlevop), intent(in) :: inlev
 integer, dimension(nlevop), intent(in) :: jlev
 real(8), dimension(nlevop), intent(in) :: elev
+integer, intent(in) :: jtot
+integer, intent(in) :: nu
+logical, intent(in) :: csflag
+logical, intent(in) :: flaghf
+logical, intent(in) :: twomol
+logical, intent(in) :: flagsu
+integer, intent(in) :: nlevop
+integer, intent(in) :: nmax
+
+
 integer, dimension(nmax) :: isc1  ! scratch array
 integer, dimension(nmax) :: isc2  ! scratch array
 real(8), dimension(nmax) :: sc2   ! scratch array
 
-logical csflag, flaghf, flagsu, twomol
+integer :: i, icol, irow, j, jj, jrow1, jrow2, ncol
+real(8) :: denrow, fak, pj, xjtot, xj2, xjrow1, xjrow2
 !
 ASSERT(row_bqs%length == nopen)
 ncol = col_bqs%length

lib/hidifcrs.F90
@@ -35,7 +35,7 @@ subroutine difcrs(fname1,a,ihomo,flaghf)
 !  FOR THE BASIS TYPE ARE IN MEMORY                             *
 !                                                               *
 !---------------------------------------------------------------*
-use constants
+use constants, only: ang2c, econv, pi, xmconv
 use mod_codim, only: mmax
 use mod_cojhld, only: jlev => jhold ! jlev(1)
 use mod_coisc1, only: inlev => isc1 ! inlev(1)

lib/hihypxsc.F90
@@ -420,7 +420,7 @@ end subroutine fill_hf
 ! This subroutine computes XS from T matrix elements 
 !************************************************************************************************
 subroutine compute_xs(twmol, rmu, ered, spins, hf, sigma)
-  use constants, only: ang2 => ang2c
+  use constants, only: ang2c
   implicit none
   ! Arguments
   logical, intent(in) :: twmol
@@ -432,7 +432,7 @@ subroutine compute_xs(twmol, rmu, ered, spins, hf, sigma)
   real(8) :: fak, dencol, denrow, ffi, fff
   integer :: ij2, ij2p, i, ii
 
-  fak = acos(-1.d0) * ang2 / (2.0d0 * rmu)
+  fak = acos(-1.d0) * ang2c / (2.0d0 * rmu)
 
 if (.not. spins%two) then
   !$OMP PARALLEL DO PRIVATE(i, ii, ij2, ij2p, ffi, fff, denrow, dencol)

lib/hiprsbr.F90
@@ -16,7 +16,7 @@ subroutine prsbr(flnam1, flnam2, a)
 !  revision:  27-sep-2013 by p. dagdigian (relax requirement that jfinl1 = jfinl2)
 !  revision:  18-aug-2015 by p. dagdigian (skip in sums if xf6j's equal zero)
 ! -------------------------------------------------------------------------
-use constants
+use constants, only: ang2c, econv
 use mod_codim, only: mmax
 use mod_coisc1, only: jlev1 => isc1 ! jlev1(1)
 use mod_coisc2, only: jlev2 => isc2 ! jlev2(1)
@@ -35,10 +35,13 @@ use mod_hiutil, only: gennam, mtime, gettim
 use mod_hiutil, only: xf6j
 use mod_hismat, only: sread, rdhead, sinqr
 use mod_hitypes, only: bqs_type
-implicit double precision (a-h, o-z)
+implicit none
+character*(*), intent(in) :: flnam1
+character*(*), intent(in) :: flnam2
+real(8), intent(in) :: a(12)
+
 type(bqs_type) :: row_bqs
 type(bqs_type) :: packed_bqs
-character*(*) flnam1, flnam2
 character*20  cdate1, cdate2
 character*40  smtfil1, smtfil2, prtfil
 character*10  elaps, cpu
@@ -49,8 +52,21 @@ logical csflg1, flghf1, flgsu1, twmol1, nucrs1, &
         batch, fast, lpar2, lpar, exstfl, diagst, &
         diagj, diagin, &
         diagjp, daginp, diag, diagp
-
-dimension a(12)
+real(8) :: aimagp
+real(8) :: cpu0, cpu1
+real(8) :: ecoll1, ecoll2
+real(8) :: ela0, ela1
+real(8) :: ered1, ered2, etrans
+real(8) :: facj, facjpp, factor
+integer :: flg, flgp  ! graffy: used to be real(8) ????
+integer :: i, iaddr, ialloc, icol, icolp, iener1, iener2, ierr, ii, iip, in1, in1p, in2, in2p, ipartx, ipower, irow, irowp
+integer :: j1, j1p, j2, j2p, jfinl1, jfinl2, jfrst1, jfrst2, jj1, jj1p, jj2, jj2p, jlp, jlpar, jlpar1, jlpar2, jlparp, jlpmx1, jlpmx2, jlpp
+integer :: jtot, jtot1, jtot2, jtotd1, jtotd2, jtotmx, jtotp, jtpmax, jtpmin
+integer :: k, l1, l1p, l2, l2p, len2, lenfs, lenpb, lngth1, lngth2
+integer :: m1chmx, m1jtot, m2chmx, m2jtot, maxjtot, mchmx, mmax2
+integer :: nlevel1, nlevel2, nlvop1, nlvop2, nnout1, nnout2, nopen, nu1, nu2, nud1, nud2, numax1, numax2, numin1, numin2
+real(8) :: phase, prefac, realp, rmu1, rmu2, sigmai, sigmar, spin, sqj1j1p
+real(8) :: x61, x62, xj1, xj1p, xj2, xj2p, xjtot, xjtotp, xjtpm, xk, xl1, xl1p, xl2, xl2p
 !
 double precision, dimension(:), allocatable :: sreal, simag
 ! storage for s-matrix elements:

lib/hitensor.F90
@@ -1606,7 +1606,7 @@ use mod_cosc1, only: elev => sc1 ! elev(1)
 use mod_coz, only: xm1lab => z_as_vec ! xm1lab(1)
 use mod_cow, only: xm2lab => w_as_vec ! xm2lab(1)
 use mod_cozmat, only: sigma => zmat_as_vec ! sigma(1)
-use constants, only: econv, xmconv, ang2c
+use constants, only: econv, xmconv
 use mod_par, only: batch
 use mod_parpot, only: potnam=>pot_name, label=>pot_label
 use mod_mom, only: spin, xj1,xj2, j1, in1, j2, in2, maxjt, maxjot, nwaves, jfsts, jlparf, jlpars, njmax, j1min, j2max

commit 4f375f967fcfa6e77736c91348e34839577d0f8f

fixed regression introduced in commit cdc62a68520c11378b348ea36dd0e7620b6c0910

lib/histmix.F90
@@ -32,7 +32,7 @@ use mod_coisc9, only: inlevt => isc9 ! inlevt(1)
 use mod_cosc1, only: elev1 => sc1 ! elev1(1)
 use mod_cosc2, only: elev2 => sc2 ! elev2(1)
 use mod_cosc3, only: elevt => sc3 ! elevt(1)
-use constants, only: econv, xmconv
+use constants, only: econv, xmconv, ang2c
 use mod_parpot, only: potnam=>pot_name, label=>pot_label
 use mod_hiutil, only: gennam, mtime, gettim
 use mod_hismat, only: sread, rdhead, sinqr
@@ -580,7 +580,7 @@ end do
 !     compute cross sections for sums of squares of t-matrix elements
 !
 !     transitions to/from unperturbed singlet levels
-fak = acos(-1.d0) * ang2 / (2.d0 * rmu1)
+fak = acos(-1.d0) * ang2c / (2.d0 * rmu1)
 do is = 1,nlevels
    isub = iptsng(is)
    xj = jlev1(isub)