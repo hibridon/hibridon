hibridon/hibridon

commit 17d7591f1ee0f77e9f7e6a3107d0388eb113bfcc

fixed error `get_elapsed_seconds` has no implicit type
note: this error showed that the previous working code was using the intrinsics `second` instead of the function `second`. So, as the function `second` (which was recently renamed as `get_elapsed_seconds` to avoid confusion) was not actually used, I deleted it

lib/hibrid4.F90
@@ -640,7 +640,7 @@ subroutine compute_wave_and_fluxes(filnam, iflux, iprint, thresh, factr, inchj, 
 use mod_codim, only: nmax => mmax
 use mod_cosout, only: nnout, jout
 use mod_coiout, only: niout, indout
-use constants
+use constants, only: zero, one
 use mod_coqvec, only: nphoto
 use mod_coeint, only: eint
 use mod_coamat, only: psir ! psir(100) psir(nopen,nopen)
@@ -692,9 +692,7 @@ type(bqs_type) :: bqs
 ipol=>ispar(3)
 
 
-one=1.d0
 onemin=-1.d0
-zero=0.d0
 ! initialize timer
 call mtime(cpu0,ela0)
 ! input

lib/hibrid1.F90
@@ -205,7 +205,7 @@ real(8) :: r2, twormu, vv0, wmax, wmin
 !     if (r.le.3.41) icount=0
 !ABER
 !  calculate coefficients of each angular term
-cpupot = cpupot - get_elapsed_seconds()
+cpupot = cpupot - second()
 call pot( vv0, r)
 
 !  vv0 is the isotropic term in the potential
@@ -358,7 +358,7 @@ rtmn = r
 rtmx = r
 90 continue
 
-cpupot = cpupot + get_elapsed_seconds()
+cpupot = cpupot + second()
 ! here for 2s-2p scattering
 ! fill in upper triangle of w matrix
 !  first fill in upper half of original matrix

lib/hiflow.F90
@@ -665,7 +665,7 @@ call soutpt (z, w, zmat, amat, &
              wrsmat, wrpart, prpart, wrxsec, prxsec, twomol, &
              nucros, firstj, nlevel, nlevop, nopen, nchtop, &
              twojlp, jlpold)
-cpuout = cpuout + get_elapsed_seconds() - t11
+cpuout = cpuout + second() - t11
 
 !  on return from soutpt:
 !     if wrxsec,prxsec, wrpart, and prpart are all .false., the upper-left

lib/hitensor.F90
@@ -2529,7 +2529,7 @@ lmax = nint(xjtot+j2mx+spin+1.d0)
 lmaxp = nint(xjtotp+j2mxp+spin+1.d0)
 kpmin=jminjp
 kmin=abs(kpmin-n)
-t1=get_elapsed_seconds()
+t1=second()
 do 70 icol=1,packed_bqs%length
 ! modify next if statement in include test for in2 (pjd)
 !      if(packed_bqs%inq(icol).ne.in2) goto 70
@@ -2545,7 +2545,7 @@ kkp=kkp+1
 xkp=kp
 65 f6a(kkp,j2,lmax-l2) = xf6j(xkp,xjtotp,xjtot,xl2,xj2,xj2)
 70 continue
-t6j = t6j + get_elapsed_seconds() - t1
+t6j = t6j + second() - t1
 kpmx=kpmax
 ! clear iadr array (pjd)
 do 79 j=0,jmx
@@ -2606,7 +2606,7 @@ do 400 irow = 1, packed_bqs%length
    kmax=nint(2*xj1)
    factor=denrow*facjjp
    ll=0
-   t1 = get_elapsed_seconds()
+   t1 = second()
 do 100 l1p=l1pmin,l1pmax,2
    ll=ll+1
    xl1p = l1p
@@ -2634,7 +2634,7 @@ do 100 l1p=l1pmin,l1pmax,2
    if(mod(ipower,2).ne.0) f9pha(ik) = -switch
 90 continue
 100 continue
-   t9j = t9j + get_elapsed_seconds() - t1
+   t9j = t9j + second() - t1
 !
 ! compute all transitions separately, unlike in SIGK
 do 300 icol = 1, packed_bqs%length

lib/hiutil.F90
@@ -603,29 +603,6 @@ te=result
 return
 end
 !------------------------------------------------------------------------
-double precision function get_elapsed_seconds()
-implicit none
-#if defined(HIB_UNIX_IFORT) || defined(HIB_UNIX_PGI) || defined(__GFORTRAN__)
-  real*4 :: te
-  call cpu_time(te)
-  get_elapsed_seconds=te
-#elif defined(HIB_UNIX_XLF)
-  get_elapsed_seconds=timef()*1000d0
-#elif defined(HIB_ULTRIX_DEC)
-  real :: et(2)
-  real(8) :: fe
-  fe=etime(et(1),et(2))
-  get_elapsed_seconds=fe
-#elif defined(__G95__)
-  real*4 :: tarray(2), result
-  call dtime(tarray,result)
-  get_elapsed_seconds=tarray(1)
-#else
-#error "This fortran compiler is not handled in this function"
-#endif
-  return
-end
-!------------------------------------------------------------------------
 subroutine gettim(sec, time)
 implicit none
 !

commit 296ff16d8221f212a07ddf1a46b9afe3d1a12d19

fixed bug introduced in commit 17d7591f1ee0f77e9f7e6a3107d0388eb113bfcc

lib/hibrid4.F90
@@ -640,7 +640,7 @@ subroutine compute_wave_and_fluxes(filnam, iflux, iprint, thresh, factr, inchj, 
 use mod_codim, only: nmax => mmax
 use mod_cosout, only: nnout, jout
 use mod_coiout, only: niout, indout
-use constants, only: zero, one
+use constants, only: zero, one, econv, xmconv
 use mod_coqvec, only: nphoto
 use mod_coeint, only: eint
 use mod_coamat, only: psir ! psir(100) psir(nopen,nopen)