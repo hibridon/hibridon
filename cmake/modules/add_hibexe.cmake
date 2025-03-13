hibridon/hibridon

commit ba8d51d224ebe70a7b3d186f0e4e1a173f8aa272

removed `-fno-automatic compilation` flag to fix gfortran warnings f951: > f951: Warning: Flag ‘-fno-automatic’ overwrites ‘-frecursive’ implied by ‘-fopenmp’
- the side effect of this fix is that local variables no longer have the save attribute, unless explicitely tagged as such.
- I explicitely added the save attribute on the local variables that require it (typically variables named `first`; I renamed them `first_time` to be more self-explanatory). I hope that I didn't forget some variables.

work related to issue #174

cmake/modules/add_hibexe.cmake
@@ -26,7 +26,7 @@ if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
       PUBLIC
       # Non-specific options
       -std=legacy                               # Allow pre-Fortran 77 syntax (e.g. arbitrary length arrays)
-      -fno-automatic                            # Put everything on the heap
+      # -fno-automatic                            # Put local variables on the heap instead of the stack. As a side effect, all affected variables receive the save attribute (persistance between calls). This flag conflicts with -fopenmp (f951: Warning: Flag ‘-fno-automatic’ overwrites ‘-frecursive’ implied by ‘-fopenmp’)
       $<$<PLATFORM_ID:Linux>:-mcmodel=large>    # Required on Linux
       # Config-specific options: RELEASE
       $<$<CONFIG:RELEASE>:-O3>                  # Optimization level at 3 for Release

lib/hiflow.F90
@@ -106,7 +106,7 @@ character*10 :: timew, cpubaw, cpuptw, cpuaiw, cpuldw, cpusmw, cpuouw, &
              cpuphw, timew1, timew2, time1, time2
 logical :: clist, firstj, ready
 !  -------------------------------------------------------------
-logical :: first
+logical :: first_time = .true.
 
 logical :: twojlp
 data twojlp / .false. /
@@ -133,10 +133,9 @@ integer :: nsteps
 
 type(bqs_type) :: bqs
 
-first=.true.
 !  get default data
 call set_default_params
-1 call hinput(first)
+1 call hinput(first_time)
 cpupt=0
 #if defined(HIB_UNIX_DEC) || defined(HIB_UNIX_IRIS)
 ttim(1)=0.d0

lib/hinput.F90
@@ -61,7 +61,7 @@ module mod_hinput
   character(len=8), parameter :: bascod(1) = ['BASISTYP']
 
 contains
-subroutine hinput(first)
+subroutine hinput(first_time)
 !  subroutine to redefine system independent input parameters for
 !  hibridon code
 !  author:  h.-j. werner
@@ -142,6 +142,7 @@ use mod_hypxsc, only: hypxsc
 implicit none
 !  iicode is the number of integer pcod's
 !  ircode is the number of real pcod's
+logical, intent(inout) :: first_time
 integer, parameter :: lcode = LPAR_COUNT
 integer, parameter :: iicode = IPAR_COUNT
 integer, parameter :: ircode = RPAR_COUNT
@@ -157,7 +158,7 @@ character*8 empty_var_list(0)
 !  pcod, fcod, and bcod)
 character*8 codex(15)
 integer nerg
-logical existf, first, openfl
+logical existf, openfl
 logical logp, opti, jtrunc
 real(8) :: a(15)
 integer :: ia(10)
@@ -282,13 +283,13 @@ ijcode=icode
 if(com) open(unit=1312, status='old', file=trim(com_file))
 
 !   define system dependent parameter codes
-if(first) then
+if(first_time) then
    islcod=0
    isrcod=0
    isicod=0
    izero=0
    call sysdat(irpot, lpar(LPAR_READPT), izero)
-   first = .false.
+   first_time = .false.
    call version(6)
 !  in this next statement the $ sign implies no line feed
 !  replace this with an equivalent formatting character if your system

tests/cph2/pot_cph2.F90
@@ -409,12 +409,11 @@ double precision function frecip(y,mq2m,nx,xgr,coef)
 !
 implicit double precision(a-h,o-z)
 dimension xgr(nx),coef(nx)
-logical first
+logical, save :: first_time = .true.
 save fac1,fac2
-data first/.true./
 !
-if (first) then
-first  = .false.
+if (first_time) then
+first_time  = .false.
   fac1 = 4.d0 / ((mq2m+1.d0)*(mq2m+2.d0))
   fac2 = (mq2m+1.d0) / (mq2m+3.d0)
 end if

tests/h2ohe/pot_h2ohe.F90
@@ -543,10 +543,9 @@ end
   use mod_h2ohe, only: lmaxli
   use mod_h2ohe, only: lmaxi, lmaxd
   implicit real*8 (a-h,o-z)
-  logical first
+  logical, save :: first_time = .true.
   dimension rpt(6)
   data a0 /0.529177249d0/
-  data first /.true./
   data xkcal /627.51d0/
   data cm    /219474.63d0/
   data icount /0/
@@ -584,12 +583,12 @@ end
   if(iaa.eq.2) rpt(1) = r
   rpt(2) = d2rad*theta
   rpt(3) = d2rad*phi
-  if(first) then
+  if(first_time) then
 !          call rdmom    ! now superfluous call
     call rdcoef   ! read induction and dispersion coeffs
     call rdexch   ! read short-range exponential parms
     call rdlin    ! read short-range linear parms
-    first=.false.
+    first_time=.false.
   endif
   ndum = nex
   ipow = 2

tests/heco/pot_heco_sapt.F90
@@ -251,14 +251,13 @@ COMMON /param/ ael(0:10,0:3), bel(0:10,0:3), alel(0:10), &
                aex(0:8,0:3),  bex(0:8,0:3), alex(0:8), &
                ad(0:8,0:3),  bd(0:8), bdd(0:8), cd(6:12,0:6,0:3), &
                ai(0:10,0:3), bi(0:8), ci(6:12,0:6,0:3)
-LOGICAL first
-DATA first /.true./
+logical, save :: first_time = .true.
 SAVE
 
 !... Fill array DLFAC with log(n!)
-IF (first) THEN
+IF (first_time) THEN
   CALL filfac(dlfac, logmax)
-  first = .false.
+  first_time = .false.
 ENDIF
 
 !.. Deviation of CO bond length from equilibrium

tests/nh3h2_qma/pot_nh3h2_qma.F90
@@ -21,6 +21,11 @@
 !     ------------------------------------------------------------------
 !     THE FOLLOWING SOUBROUTINE WILL BE THE MAIN FUNCTION FOR MAKEPOT.
 !     NOTE THAT VVL IS IN HARTREES.
+
+module mod_nh3h2_qma
+   character*40 :: potfil
+end module mod_nh3h2_qma
+
 subroutine driver
 use mod_covvl, only: vvl
 use mod_hibasutil, only: raise
@@ -153,7 +158,7 @@ use mod_cosys, only: scod
 use mod_cosysi, only: nscode, isicod, ispar
 use mod_cosysr, only: isrcod, rspar
 use mod_hibasutil, only: raise
-use funit, only: FUNIT_INP
+use mod_nh3h2_qma, only: potfil
 implicit none
 !
 integer MAX_NR, MAX_NV
@@ -173,14 +178,11 @@ double precision rr(MAX_NR), v_pot(MAX_NR, MAX_NV), &
 integer, intent(inout) :: irpot
 logical, intent(inout) :: readpt
 integer, intent(in) :: iread
-character*(*) fname
 !     NUMBER OF BASIS-SPECIFIC VARIABLES, MODIFY ACCORDINGLY.
 integer icod, ircod
 parameter (icod=5, ircod=5)
 
 
-character*40 potfil
-save potfil
 integer, pointer :: nterm, ipotsy, iop, ninv, jmax, ipotsy2, j2max, j2min
 real(8), pointer :: brot, crot, delta, emax, drot
 nterm=>ispar(1); ipotsy=>ispar(2); iop=>ispar(3); ninv=>ispar(4); jmax=>ispar(5); ipotsy2=>ispar(6)
@@ -220,12 +222,31 @@ close (8)
 return
 80 call raise('error read from input file.')
 return
+end subroutine
 !     ------------------------------------------------------------------
-entry ptrusr(fname, readpt)
+subroutine ptrusr(fname, readpt)
+implicit none
+character*(*), intent(inout) :: fname
+logical, intent(inout) :: readpt
 UNUSED_DUMMY(fname)
+UNUSED_DUMMY(readpt)
 return
+end subroutine
 !     ------------------------------------------------------------------
-entry savusr(readpt)
+subroutine savusr(readpt)
+use mod_cosysi, only: ispar
+use mod_cosysr, only: rspar
+use funit, only: FUNIT_INP
+use mod_nh3h2_qma, only: potfil
+implicit none
+logical, intent(inout) :: readpt
+integer, pointer :: nterm, ipotsy, iop, ninv, jmax, ipotsy2, j2max, j2min
+real(8), pointer :: brot, crot, delta, emax, drot
+nterm=>ispar(1); ipotsy=>ispar(2); iop=>ispar(3); ninv=>ispar(4); jmax=>ispar(5); ipotsy2=>ispar(6)
+j2max=>ispar(7); j2min=>ispar(8)
+
+brot=>rspar(1); crot=>rspar(2); delta=>rspar(3); emax=>rspar(4); drot=>rspar(5)
+UNUSED_DUMMY(readpt)
 !     WRITE THE LAST FEW LINES OF THE INPUT FILE.
 write (FUNIT_INP, 220) ipotsy, iop, ninv, ipotsy2
 220 format (4i4, 14x,'   ipotsy, iop, ninv, ipotsy2')

tests/ohh2_bausr/pot_ohh2_bausr.F90
@@ -51,6 +51,7 @@ real(8) :: splf_d(MAX_NR, MAX_NVF)
 !     Machine epsilon
 real(8), parameter :: machep=epsilon(0d0)
 
+character*40 :: potfil
 end module
 !     ------------------------------------------------------------------
 !     THE FOLLOWING SOUBROUTINE WILL BE THE MAIN FUNCTION FOR MAKEPOT.
@@ -195,19 +196,15 @@ use mod_cosys, only: scod
 use mod_cosysi, only: nscode, isicod, ispar
 use mod_cosysr, only : isrcod, rspar
 use mod_hibasutil, only: raise
-use funit, only: FUNIT_INP
+use mod_pot_ohh2_bausr, only: potfil
 !
 implicit none
 integer, intent(inout) :: irpot
 logical, intent(inout) :: readpt
 integer, intent(in) :: iread
-character*(*) fname
 !     NUMBER OF BASIS-SPECIFIC VARIABLES, MODIFY ACCORDINGLY.
 integer icod, ircod
 parameter (icod=5, ircod=5)
-
-character*40 potfil
-save potfil
 
 integer, pointer :: j1max, npar, j2min, j2max, iptsy2
 real(8), pointer :: brot, aso, p, q, drot
@@ -215,6 +212,7 @@ real(8), pointer :: brot, aso, p, q, drot
 j1max=>ispar(1); npar=>ispar(2); j2min=>ispar(3); j2max=>ispar(4); iptsy2=>ispar(5)
 brot=>rspar(1); aso=>rspar(2); p=>rspar(3); q=>rspar(4); drot=>rspar(5)
 UNUSED_DUMMY(irpot)
+UNUSED_DUMMY(readpt)
 !     DEFINE THE NAMES HERE
 scod(1)='J1MAX'
 scod(2)='NPAR'
@@ -242,13 +240,30 @@ close (8)
 return
 80 call raise('error read from input file.')
 return
+end subroutine
 !     ------------------------------------------------------------------
-entry ptrusr(fname, readpt)
+subroutine ptrusr(fname, readpt)
+implicit none
+character*(*), intent(inout) :: fname
+logical, intent(inout) :: readpt
 UNUSED(fname)
 UNUSED(readpt)
 return
+end subroutine
 !     ------------------------------------------------------------------
-entry savusr(readpt)
+subroutine savusr(readpt)
+use mod_pot_ohh2_bausr, only: potfil
+use mod_cosysi, only: ispar
+use mod_cosysr, only : rspar
+use funit, only: FUNIT_INP
+implicit none
+logical, intent(inout) :: readpt
+integer, pointer :: j1max, npar, j2min, j2max, iptsy2
+real(8), pointer :: brot, aso, p, q, drot
+UNUSED_DUMMY(readpt)
+
+j1max=>ispar(1); npar=>ispar(2); j2min=>ispar(3); j2max=>ispar(4); iptsy2=>ispar(5)
+brot=>rspar(1); aso=>rspar(2); p=>rspar(3); q=>rspar(4); drot=>rspar(5)
 !     WRITE THE LAST FEW LINES OF THE INPUT FILE.
 write (FUNIT_INP, 230) j1max, npar
 230 format (2i4, 22x, '   j1max, npar')

commit 8759b5347da8e0ca5143a2666bf62fa4c0d7a2d6

reverted regression introduced in commit ba8d51d224ebe70a7b3d186f0e4e1a173f8aa272
This regression caused the config-`DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DCMAKE_Fortran_COMPILER=gfortran-11  -DBLA_VENDOR=Apple`
to fail on macos-11.2

notes:
- all other 7 configs in workflows/quick-<os>.yml work
- the config with `-DBLA_VENDOR=Intel10_64lp` instead of `-DBLA_VENDOR=Apple` works too
- I investigated but the crashes are nasty: despite all debug symbols are here, the stack is lost after the crash in gdb
- when investigating h2ohe_orig test crash (most tests actually pass), the crash happen at the instruction `ntop = nchtop` in `hiflow.F90:631`, which seemed fine to me. This seems to indicate a stack corruption that happened before.

work related to issue #174

cmake/modules/add_hibexe.cmake
@@ -26,7 +26,7 @@ if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
       PUBLIC
       # Non-specific options
       -std=legacy                               # Allow pre-Fortran 77 syntax (e.g. arbitrary length arrays)
-      # -fno-automatic                            # Put local variables on the heap instead of the stack. As a side effect, all affected variables receive the save attribute (persistance between calls). This flag conflicts with -fopenmp (f951: Warning: Flag ‘-fno-automatic’ overwrites ‘-frecursive’ implied by ‘-fopenmp’)
+      -fno-automatic                            # Put local variables on the heap instead of the stack. As a side effect, all affected variables receive the save attribute (persistance between calls). This flag conflicts with -fopenmp (f951: Warning: Flag ‘-fno-automatic’ overwrites ‘-frecursive’ implied by ‘-fopenmp’)
       $<$<PLATFORM_ID:Linux>:-mcmodel=large>    # Required on Linux
       # Config-specific options: RELEASE
       $<$<CONFIG:RELEASE>:-O3>                  # Optimization level at 3 for Release