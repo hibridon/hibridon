hibridon/hibridon

commit 61b1ac8ac941cb55b93c6e3e615542e05e6a34a9

fixed last warnings in pot_vfit
- this involved the addition of interfaces for blas subroutines  and user provided potential subroutines

work related to issue #174

lib/hiblas.F90
@@ -1,0 +1,15 @@
+! interfaces to blas and lapack subroutines
+module mod_hiblas
+  interface
+
+    function ddot(n, dx, incx, dy, incy)
+      integer, intent(in) :: n
+      real(8), intent(in) :: dx(:)
+      integer, intent(in) :: incx
+      real(8), intent(in) :: dy(:)
+      integer, intent(in) :: incy
+      real(8) :: ddot
+    end function
+
+  end interface
+end module mod_hiblas

lib/CMakeLists.txt
@@ -18,6 +18,7 @@ add_library(hib
     hiamp.F90
     hibasis.F90
     hibasutil.F90
+    hiblas.F90
     hibound.F90
     hibrid1.F90
     hibrid2.F90
@@ -33,6 +34,7 @@ add_library(hib
     himem.F90
     hinput.F90
     hiparcst.F90
+    hipot.F90
     hiprsbr.F90
     hismat.F90
     histmix.F90

lib/hipot.F90
@@ -1,0 +1,16 @@
+module mod_hipot
+  ! provides interfaces to user-provided subroutines related to potential
+  interface
+
+    subroutine pot(vv0, r)
+      real(8), intent(out) :: vv0
+      real(8), intent(in) :: r
+    end subroutine
+
+    subroutine loapot(iunit, filnam)
+      integer, intent(in) :: iunit  ! if a data file is used, this subroutine is expected to use this unit to open it in read mode (not used here)
+      character*(*), intent(in) :: filnam  ! if a data file is used, the file name of the data file
+    end subroutine
+
+  end interface
+end module mod_hipot

tests/vfit/pot_vfit.F90
@@ -80,6 +80,7 @@ contains
   use mod_copot, only: maxang
   use mod_coptx, only: rex, rin, tanhy, fex
   use mod_cofit, only: maxpws, minmps, maxmps, mpsstp, idimp, idimr
+  use mod_hiblas, only: ddot
   use, intrinsic :: ISO_C_BINDING   ! for C_LOC and C_F_POINTER
   implicit double precision(a-h,o-z)
   real(8), intent(in) :: rr
@@ -89,7 +90,6 @@ contains
   real(8), intent(out) :: avec(10)
   integer, intent(inout) :: iblkx
 
-  real(8) :: ddot
   real(8) :: rvecpp(10)  ! scratch array
   real(8), target :: b(10,10)  ! scratch array
 
@@ -118,9 +118,9 @@ contains
   do 100 n = 1, idimp
   iblk =iblk +1
   iblkx=iblkx+1
-  b(n,l)=fex(iblkx)*ddot(ne,a(2,iblk),1,rex,1)
+  b(n,l)=fex(iblkx)*ddot(ne,a(2:,iblk),1,rex,1)
   if(nm.ne.0) b(n,l)=b(n,l) &
-      -tanhy(iblkx)*ddot(nm,a(m1,iblk),1,rin(minmps),mpsstp)
+      -tanhy(iblkx)*ddot(nm,a(m1:,iblk),1,rin(minmps:),mpsstp)
   100 continue
   200 continue
   !
@@ -303,6 +303,7 @@ end
 subroutine driver
 use mod_covvl, only: vvl
 use mod_par, only: readpt
+use mod_hipot, only: pot, loapot
 implicit none
 character *48 potnam
 character *40 filnam
@@ -343,11 +344,11 @@ subroutine pot(vv0,r)
 ! -------------------------------------------------------------------
 use mod_covvl, only: vvl
 use mod_cosysi, only: ispar
-use mod_parbas, only: maxtrm, maxvb2, ntv, lammin, lammax
+use mod_parbas, only: ntv, lammin, lammax
 use mod_selb, only: ibasty
 use mod_skip, only: nskip
 use mod_coptx, only: nblkx, maxpwx, minmpx, maxmpx, rex, rin, tanhy, fex, a1, ah, art, are
-use mod_copot, only: maxang, maxnr, nc, a, maxpw, minms, maxms, msstp, mdimp, pinv, mdimr, vibmat, avec
+use mod_copot, only: nc, a, maxpw, minms, maxms, msstp, mdimp, pinv, mdimr, vibmat, avec
 use mod_cofit, only: npa, maxpws, minmps, maxmps, mpsstp, idimp, idimr
 use mod_vfit, only: vcalc
 implicit none

commit a0d8a28ed6a52966c125fcc3c6ab50c811ec9ed7

fixed bug introduced in commit 61b1ac8ac941cb55b93c6e3e615542e05e6a34a9 that cased vfit tests to fail

lib/hiblas.F90
@@ -4,9 +4,9 @@ module mod_hiblas
 
     function ddot(n, dx, incx, dy, incy)
       integer, intent(in) :: n
-      real(8), intent(in) :: dx(:)
+      real(8), intent(in) :: dx(*)
       integer, intent(in) :: incx
-      real(8), intent(in) :: dy(:)
+      real(8), intent(in) :: dy(*)
       integer, intent(in) :: incy
       real(8) :: ddot
     end function
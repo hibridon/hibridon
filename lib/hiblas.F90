a0d8a28ed6a52966c125fcc3c6ab50c811ec9ed7
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2025-03-26 17:05:21 +0100

    fixed bug introduced in commit 61b1ac8ac941cb55b93c6e3e615542e05e6a34a9 that cased vfit tests to fail

--- lib/hiblas.F90
+++ lib/hiblas.F90
@@ -4,9 +4,9 @@ module mod_hiblas
 
     function ddot(n, dx, incx, dy, incy)
       integer, intent(in) :: n
-      real(8), intent(in) :: dx(:)
+      real(8), intent(in) :: dx(*)
       integer, intent(in) :: incx
-      real(8), intent(in) :: dy(:)
+      real(8), intent(in) :: dy(*)
       integer, intent(in) :: incy
       real(8) :: ddot
     end function
