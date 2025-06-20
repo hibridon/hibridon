hibridon/hibridon

commit 3bcf627cf73a588362ba0d5546c820e0c711654b

refactor: turned code that writes data in wfu file in airprp subroutine into a subroutine write_airy_record
- this makes the code more readable
- turned global scratch array sc10 into a local variable

work related to issue #201

lib/hiwav.F90
@@ -443,4 +443,58 @@ call exit
 return
 end
 
+subroutine write_airy_record(wfu_file, rlast, drnow, nch, eigold, writs, nmax, z, vecnow, y1, y2, y4, gam1, lmuab)
+implicit none
+type(wfu_file_type), allocatable, intent(inout) :: wfu_file
+real(8), intent(in) :: rlast
+real(8), intent(in) :: drnow
+integer, intent(in) :: nch
+real(8), intent(in) :: eigold(nch)
+logical, intent(in) :: writs
+integer, intent(in) :: nmax
+real(8), intent(in) :: z(nch*nmax)  ! this is G(n-1,n) in the local basis
+real(8), intent(in) :: vecnow(nch*nmax)  ! vecnow is transformation from free basis into local basis in first interval
+real(8), intent(in) :: y1(nch)
+real(8), intent(in) :: y2(nch)
+real(8), intent(in) :: y4(nch)
+real(8), intent(in) :: gam1(nch)
+real(8), intent(in) :: lmuab(nch)
+
+integer :: lrairy
+integer :: i, ich, icol
+
+  ASSERT(allocated(wfu_file))
+  wfu_file%irec = wfu_file%irec + 1
+  write (wfu_file%ifil, err=950) -rlast, drnow
+  !     Adiabatic energies
+  write (wfu_file%ifil, err=950) (eigold(i), i=1, nch)
+  !     The following information will not be written if writs set to F
+  if (writs) then
+    icol = 1
+    do ich = 1, nch
+       write (wfu_file%ifil, err=950) (z(icol - 1 + i), i=1, nch)
+       icol = icol + nmax
+    end do
+    icol = 1
+    do ich = 1, nch
+       write (wfu_file%ifil, err=950) (vecnow(icol - 1 + i), i=1, nch)
+       icol = icol + nmax
+    end do
+    !
+    write (wfu_file%ifil, err=950) (y1(i), i=1, nch), (y2(i), i=1, nch), &
+         (y4(i), i=1, nch), (gam1(i), i=1, nch), &
+         (lmuab(i), i=1, nch)
+    lrairy = get_wfu_airy_rec_length(wfu_file%nchwfu, 0)
+  else
+    lrairy = get_wfu_airy_rec_length(wfu_file%nchwfu, 1)
+  end if
+  !
+  write (wfu_file%ifil, err=950) 'ENDWFUR', char(mod(wfu_file%irec, 256))
+  wfu_file%iendwv = wfu_file%iendwv + lrairy
+
+950 write (0, *) ' *** ERROR WRITING WFU FILE (AIRY). ABORT.'
+  call exit()
+
+end subroutine
+
 end module mod_wave

lib/hibrid1.F90
@@ -1342,11 +1342,10 @@ subroutine airprp (z, &
 ! ----------------------------------------------------------------------------
 use mod_coqvec, only: nphoto, q
 use mod_coqvec2, only: q2
-use mod_cosc10, only: sc10
 use mod_ancou, only: ancou_type
 use mod_hiba10_22p, only: energ22
 use mod_par, only: par_iprint=>iprint
-use mod_wave, only: wfu_file_type, get_wfu_airy_rec_length
+use mod_wave, only: wfu_file_type, get_wfu_airy_rec_length, write_airy_record
 use mod_selb, only: ibasty
 use mod_phot, only: photof, wavefn, writs
 use mod_cotq1, only: tmat2 => dpsir
@@ -1378,7 +1377,7 @@ logical, intent(in) :: iprint
 logical, intent(in) :: twoen
 logical, intent(in) :: noprin
 type(ancou_type), intent(in) :: v2
-type(wfu_file_type), intent(inout) :: wfu_file
+type(wfu_file_type), intent(inout), allocatable :: wfu_file
 logical :: airy_prop_completed
 integer i, icol, ierr, ipt, izero, kstep, maxstp, &
         ncol, npt, nskip
@@ -1407,6 +1406,8 @@ real(8), dimension(nch) :: cc
 real(8), dimension(nch) :: y4
 real(8), dimension(nch) :: gam1
 real(8), dimension(nch) :: gam2
+real(8), dimension(nch) :: lmuab  ! local mu(a,b) propagator
+
 integer(8) :: lrairy ! length of an airy record in bytes
 ! ----------------------------------------------------------------------------
 ! Save variables for subsequent energies
@@ -1563,7 +1564,7 @@ do kstep = 1, maxstp
 #endif
     ! if wavefunction desired, temporarily save local mu(a,b) propagator
     ! this is now in the first column of tmat
-    if (wavefn) call dcopy(nch,tmat,1,sc10,1)
+    if (wavefn) call dcopy(nch,tmat,1,lmuab,1)
     !  premultiply by y3 and add to existing q
     !  cc is used as scratch array here
     ind=1
@@ -1600,33 +1601,7 @@ do kstep = 1, maxstp
     ! save this matrix as well as transformation matrix
     ! into local interval and local propagators
     !
-    wfu_file%irec = wfu_file%irec + 1
-    write (wfu_file%ifil, err=950) -rlast, drnow
-    !     Adiabatic energies
-    write (wfu_file%ifil, err=950) (eigold(i), i=1, nch)
-    !     The following information will not be written if writs set to F
-    if (writs) then
-      icol = 1
-      do ich = 1, nch
-         write (wfu_file%ifil, err=950) (z(icol - 1 + i), i=1, nch)
-         icol = icol + nmax
-      end do
-      icol = 1
-      do ich = 1, nch
-         write (wfu_file%ifil, err=950) (vecnow(icol - 1 + i), i=1, nch)
-         icol = icol + nmax
-      end do
-      !
-      write (wfu_file%ifil, err=950) (y1(i), i=1, nch), (y2(i), i=1, nch), &
-           (y4(i), i=1, nch), (gam1(i), i=1, nch), &
-           (sc10(i), i=1, nch)
-      lrairy = get_wfu_airy_rec_length(wfu_file%nchwfu, 0)
-    else
-      lrairy = get_wfu_airy_rec_length(wfu_file%nchwfu, 1)
-    end if
-    !
-    write (wfu_file%ifil, err=950) 'ENDWFUR', char(mod(wfu_file%irec, 256))
-    wfu_file%iendwv = wfu_file%iendwv + lrairy
+    call write_airy_record(wfu_file, rlast, drnow, nch, eigold, writs, nmax, z, vecnow, y1, y2, y4, gam1, lmuab)
 
   end if
   !
@@ -1839,8 +1814,6 @@ deallocate(vecnew)
 
 return
 !
-950 write (0, *) ' *** ERROR WRITING WFU FILE (AIRY). ABORT.'
-call exit()
 end
 ! ----------------------------------------------------------------------
 subroutine gndloc (vecnow, rnow, drnow, nch, nmax, q)

lib/hibrid3.F90
@@ -177,7 +177,7 @@ integer, intent(in) :: nmax
 type(ancou_type), intent(in) :: v2
 integer, intent(inout) :: isteps
 integer, intent(inout) :: nsteps
-type(wfu_file_type), intent(inout) :: wfu_file
+type(wfu_file_type), intent(inout), allocatable :: wfu_file
 
 logical :: twoen
 logical ::  first

lib/himem.F90
@@ -780,16 +780,6 @@ module mod_cosc9
    end subroutine allocate_cosc9
 end module mod_cosc9
 
-module mod_cosc10
-   implicit none
-   real(8), dimension(:), allocatable :: sc10
-   contains
-   subroutine allocate_cosc10(amax)
-      integer, intent(in) :: amax
-      allocate(sc10(amax))
-   end subroutine allocate_cosc10
-end module mod_cosc10
-
 module mod_cosc11
    implicit none
    real(8), dimension(:), allocatable :: sc11

src/himain.F90
@@ -57,7 +57,6 @@ use mod_cosc6, only: allocate_cosc6
 use mod_cosc7, only: allocate_cosc7
 use mod_cosc8, only: allocate_cosc8
 use mod_cosc9, only: allocate_cosc9
-use mod_cosc10, only: allocate_cosc10
 use mod_cosc11, only: allocate_cosc11
 use mod_coeig, only: allocate_coeig
 use mod_coeig2, only: allocate_coeig2
@@ -266,7 +265,6 @@ call allocate_cosc6(kmax)
 call allocate_cosc7(kmax)
 call allocate_cosc8(kmax)
 call allocate_cosc9(kmax)
-call allocate_cosc10(kmax)
 #if defined(HIB_UNIX_DARWIN) || defined(HIB_UNIX_X86)
 call allocate_cosc11(kaux3)
 #endif

commit 397d972bb0e882f0cb68f2d9a1d2b25c263f55fa

fixed regression introduced in commit 3bcf627cf73a588362ba0d5546c820e0c711654b
work related to issue #201

lib/hiwav.F90
@@ -491,6 +491,7 @@ integer :: i, ich, icol
   !
   write (wfu_file%ifil, err=950) 'ENDWFUR', char(mod(wfu_file%irec, 256))
   wfu_file%iendwv = wfu_file%iendwv + lrairy
+  return
 
 950 write (0, *) ' *** ERROR WRITING WFU FILE (AIRY). ABORT.'
   call exit()