hibridon/hibridon

commit f0a81413338d34e1e863708f0f5a412f6bcd98dd

added a caheflux test that
 - this test covers uncovered code areas, where flux is used with base 7 (flux code contains lots of areas specific to base 7)

 work related to issue #34 and #245

tests/CMakeLists.txt
@@ -1,5 +1,27 @@
 # Add the executable that will compare output files
 add_executable(check_outputs comp_mods.f90 comp_tests.f90)
+set_compile_options(check_outputs "OFF")
+# GNU (gfortran)
+if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+    target_link_libraries(check_outputs
+      PUBLIC
+      $<$<CONFIG:DEBUG>:-fsanitize=address>         # Address sanitizer (beware: tests showed that with -fsanitize=address memory is not actually released when deallocate is called. Is this a true memory leak in address sanitizer?)
+      $<$<BOOL:${ENABLE_CODE_COVERAGE}>:--coverage> # Code coverage (same as -lgcov at link time)
+      $<$<BOOL:${ENABLE_PROFILING}>:-g>         # The profiler requires both the debug and profile directives (-g and -p)
+      $<$<BOOL:${ENABLE_PROFILING}>:-p>         # The profiler requires both the debug and profile directives (-g and -p)
+      $<$<AND:$<BOOL:${LINK_TIME_OPTIMIZATION}>,$<CONFIG:RELEASE>>:-flto>  # Activate link time optimizations so that gfortran can inline some function calls
+      #$<$<CONFIG:RELEASE>:-fopt-info>           # You can get some information from gfortran with the flag -fopt-info that will tell you about optimizations missed or performed by the compiler
+    )
+# Intel (ifort)
+elseif (Fortran_COMPILER_NAME STREQUAL "ifort")
+    target_link_libraries(check_outputs
+      PUBLIC
+      $<$<BOOL:${ENABLE_PROFILING}>:-g>         # The profiler requires both the debug and profile directives (-g and -p)
+      $<$<BOOL:${ENABLE_PROFILING}>:-p>         # The profiler requires both the debug and profile directives (-g and -p)
+      $<$<AND:$<BOOL:${LINK_TIME_OPTIMIZATION}>,$<CONFIG:RELEASE>>:-ipo>  # activate interprocediral optimization (aka link time optimization)
+    )
+endif()
+
 
 # Add all test subdirectories
 add_subdirectory(arch4)

tests/cahe/CMakeLists.txt
@@ -16,6 +16,23 @@ set(TEST_LABELS "coverage")
 add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_T_MATRIX_SIZE}" "${TEST_LABELS}")
 
 #------------------------------------------------------------------------------
+# cahe test with flux (to test flux calculations which have special cases with ibasty=7)
+#------------------------------------------------------------------------------
+
+set(TEST_ID caheflux)
+set(TEST_POT_SRC_FILE "pot_cahe.F90")
+set(TEST_POT_DATA_FILES "")
+set(TEST_COMMAND_FILE "caheflux_test.com")
+set(TEST_INPUT_FILES "Caheflux_test.inp")
+set(TEST_OUTPUT_FILES "Job.flx")
+set(TEST_KMAX 191)
+set(TEST_T_MATRIX_SIZE kmax)
+set(TEST_LABELS "coverage")
+
+
+add_hibridon_test("${TEST_ID}" "${TEST_POT_SRC_FILE}" "${TEST_POT_DATA_FILES}" "${TEST_COMMAND_FILE}" "${TEST_INPUT_FILES}" "${TEST_OUTPUT_FILES}" "${TEST_T_MATRIX_SIZE}" "${TEST_LABELS}")
+
+#------------------------------------------------------------------------------
 # nologdprop test (test case where start==rendld)
 #------------------------------------------------------------------------------
 

tests/cahe/Caheflux_test.inp
@@ -1,0 +1,29 @@
+He-Ca                        
+   7                             ibasty
+  T  T  F  F                     logdfl, airyfl, readpt, bastst
+    4.0000    20.0000    0.100   rstart, rendld, spac
+  F                              prairy
+  1.150      8.00    25.0 15.00  tolai, rincr, rendai, fstfac
+   1                             nerg
+   113.04080295
+   3.638205                      xmu
+   -30.0000                      rcut
+   2   10   1    0   0   0   1   jtot1,jtot2,jtotd,jlpar,numin,numax,nud
+  48   0                         lscreen, iprint
+   3    1                        nnout,niout
+   0  1  2
+   1
+  F  F  F  F  T                  prlogd, prsmat, prt2, t2test, wrsmat
+  F  F  F  F  T                  wrpart, prpart, prxsec, wrxsec, wavefl
+  T  T  F  F  F                  noprin, chlist, ipos, nucros, photof
+  F  F  F  T  F                  flaghf, csflag, flagsu, ihomo, twomol
+  F  F                           rsflag, boundc
+   1   0   1                     nstate, ipol, npot
+   0.000       7.076       27.44           E-3P0, 3P1, 3P2
+   183.9      1                       E-1P1, CMIX
+  1.0000E-04   9.000      0.4000       13.00       40.00     3 Pi Parameters
+  8.0000E-06   13.80      0.4700       18.00       50.00     3 Sig Parameters
+  1.0000E-04   9.000      0.4000       13.00       40.00     1 Pi Parameters
+  8.0000E-06   13.80      0.4500       18.00       50.00     1 Sig Parameters
+  3.5000E-03   6.500      0.4300      4.3000E-03             1 Sigma Morse
+   8.100      1.0000E-04  7.5000E-02                         Gaussian Coupling

tests/cahe/caheflux_test.com
@@ -1,0 +1,4 @@
+inp=Caheflux_test.inp
+run
+flux,,4
+exit

tests/comp_tests.f90
@@ -11,6 +11,10 @@ program comp_tests
     real(8)             :: tolerance
     real(8)             :: min_significant_value ! values below this are ignored in the comparison
     integer             :: num_header_lines(2)
+    character(len=200)  :: pattern
+    integer :: num_patterns
+    integer :: pattern_index
+    logical :: pattern_is_found
     ! Get test and reference files paths from command arguments
     call get_command_argument(1,ref)
     call get_command_argument(2,test)
@@ -38,23 +42,45 @@ program comp_tests
     case("xxsc"); num_header_lines = 3
     case("tcb"); num_header_lines = 2
     case ("flx") ! Header ends at first occurence of "R (BOHR) AND OUTGOING FLUXES"
-        num_header_lines(1) = get_first_occ_of("R (BOHR) AND OUTGOING FLUXES",ref)
-        num_header_lines(2) = get_first_occ_of("R (BOHR) AND OUTGOING FLUXES",test)
+        num_patterns = 2
+        pattern_is_found = .false.
+        do pattern_index=1, num_patterns
+            if (pattern_index == 1) then
+                pattern = "R (BOHR) AND OUTGOING FLUXES"
+            else if (pattern_index == 2) then
+                pattern = "TRANSFORMATION MATRIX AT R = "
+            else
+                write(Error_Unit, "(a)") "Error: Invalid pattern index."
+                stop 1
+            end if
+            write(Output_Unit, "(a)") "testing against pattern: " // trim(pattern)
+
+            call get_header_lines_counts(ref, test, pattern, num_header_lines(1), num_header_lines(2), pattern_is_found)
+            if (pattern_is_found) then
+                exit ! Exit the loop if the pattern is found
+            end if
+        end do
+
+        if (.not. pattern_is_found) then
+            write(Error_Unit, "(a)") "Error: Pattern not found in either reference or test file."
+            stop 1
+        end if
+
         ! the values of the flx files can be highly sensitive to compilers (see doc/hib_html/tests.html and issue #37)
         ! so we ignore more values
         min_significant_value = 1e-10
     case("evl") ! Header ends at first occurence of "** EIGENVALUES"
-        num_header_lines(1) = get_first_occ_of("** EIGENVALUES",ref)
-        num_header_lines(2) = get_first_occ_of("** EIGENVALUES",test)
+        pattern = "** EIGENVALUES"
+        call get_header_lines_counts(ref, test, pattern, num_header_lines(1), num_header_lines(2), pattern_is_found)
     case("xsc") ! Header ends at first occurence of "INTEGRAL CROSS SECTIONS"
-        num_header_lines(1) = get_first_occ_of("INTEGRAL CROSS SECTIONS",ref)
-        num_header_lines(2) = get_first_occ_of("INTEGRAL CROSS SECTIONS",test)
+        pattern = "INTEGRAL CROSS SECTIONS"
+        call get_header_lines_counts(ref, test, pattern, num_header_lines(1), num_header_lines(2), pattern_is_found)
     case("stdout") ! Header ends at first occurence of "Hibridon>"
-        num_header_lines(1) = get_first_occ_of("Hibridon>",ref)
-        num_header_lines(2) = get_first_occ_of("Hibridon>",test)
+        pattern = "Hibridon>"
+        call get_header_lines_counts(ref, test, pattern, num_header_lines(1), num_header_lines(2), pattern_is_found)
     case("out")
-        num_header_lines(1) = get_first_occ_of("** LABEL:",ref)
-        num_header_lines(2) = get_first_occ_of("** LABEL:",test)
+        pattern = "** LABEL:"
+        call get_header_lines_counts(ref, test, pattern, num_header_lines(1), num_header_lines(2), pattern_is_found)
     end select
 
     ! Compare numeric values between reference and test files
@@ -63,23 +89,54 @@ program comp_tests
 
 contains 
 
-! This function returns the line where the first occurence of a substring is find.
-! If the substring is not detected, STOP 1.
-function get_first_occ_of(sub,file) result(iline)
+! This function returns the line where the first occurence of a pattern is find.
+! If the pattern is not detected, pattern_is_found is set to .false
+function get_first_occ_of(pattern, file_path, pattern_is_found) result(iline)
     implicit none
-    character(len=*), intent(in) :: sub, file
+    character(len=*), intent(in) :: pattern
+    character(len=*), intent(in) :: file_path
+    logical, intent(out) :: pattern_is_found
     integer :: iline, ierr
     character(len=200) :: line
     iline = 0
-    open(unit=1, file=file, status='old')
+    open(unit=1, file=file_path, status='old')
     do
         read(1,'(a)', iostat=ierr) line ; if(ierr.ne.0) exit ! Try to read the current line. If it fails, exit the do loop.
         iline = iline+1 ! Increment the line number
-        if (index(line, trim(sub)).ne.0) then ; close(1) ; return ; endif ! If the substring is found, close the file and return.
+        if (iline == 49) then
+            write(Error_Unit, "(a)") "line=" // trim(line)
+        end if
+        if (index(line, trim(pattern)).ne.0) then
+            ! If the substring is found, close the file and return.
+            close(1)
+            pattern_is_found = .true.
+            return
+        endif
     enddo
     close(1)
-    STOP 1 ! If the function did not return within the do loop, then the substring is absent -> STOP 1.
+    pattern_is_found = .false. ! If the substring was not found, set the flag to false.
 end function get_first_occ_of
+
+subroutine get_header_lines_counts(ref_data_file_path, test_data_file_path, data_line_pattern, num_ref_header_lines, num_test_header_lines, pattern_is_found)
+implicit none
+character(len=*), intent(in) :: ref_data_file_path  ! eg "/work/hibridon/issues/issue249/hibridon.git/tests/cahe/Job.flx"
+character(len=*), intent(in) :: test_data_file_path  ! eg "/work/hibridon/issues/issue249/build/tests/cahe/Job.flx"
+character(len=*), intent(in) :: data_line_pattern  ! Pattern for the first data line, eg "R (BOHR) AND OUTGOING FLUXES"
+integer, intent(out) :: num_ref_header_lines
+integer, intent(out) :: num_test_header_lines
+logical, intent(out) :: pattern_is_found
+  num_ref_header_lines = get_first_occ_of(data_line_pattern, ref_data_file_path, pattern_is_found)
+  if (.not. pattern_is_found) then
+    write(Error_Unit, "(a)") "Pattern not found in reference file " // trim(ref_data_file_path)
+    return
+  end if
+  num_test_header_lines = get_first_occ_of(data_line_pattern, test_data_file_path, pattern_is_found)
+  if (.not. pattern_is_found) then
+    write(Error_Unit, "(a)") "Pattern not found in test file " // trim(test_data_file_path)
+    return
+  end if
+end subroutine
+
 
 
 

commit a80e754e80368c4302b6f07f798555966fa8eb02

fixed regression introduced in commit f0a81413338d34e1e863708f0f5a412f6bcd98dd
 - removed the elaborate compilation options on utilities because they caused their build to fail because of warnings. Anyway they are not as important as on hibridon's code

tests/CMakeLists.txt
@@ -1,27 +1,11 @@
 # Add the executable that will compare output files
 add_executable(check_outputs comp_mods.f90 comp_tests.f90)
-set_compile_options(check_outputs "OFF")
-# GNU (gfortran)
 if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
-    target_link_libraries(check_outputs
+    target_compile_options(check_outputs
       PUBLIC
-      $<$<CONFIG:DEBUG>:-fsanitize=address>         # Address sanitizer (beware: tests showed that with -fsanitize=address memory is not actually released when deallocate is called. Is this a true memory leak in address sanitizer?)
-      $<$<BOOL:${ENABLE_CODE_COVERAGE}>:--coverage> # Code coverage (same as -lgcov at link time)
-      $<$<BOOL:${ENABLE_PROFILING}>:-g>         # The profiler requires both the debug and profile directives (-g and -p)
-      $<$<BOOL:${ENABLE_PROFILING}>:-p>         # The profiler requires both the debug and profile directives (-g and -p)
-      $<$<AND:$<BOOL:${LINK_TIME_OPTIMIZATION}>,$<CONFIG:RELEASE>>:-flto>  # Activate link time optimizations so that gfortran can inline some function calls
-      #$<$<CONFIG:RELEASE>:-fopt-info>           # You can get some information from gfortran with the flag -fopt-info that will tell you about optimizations missed or performed by the compiler
-    )
-# Intel (ifort)
-elseif (Fortran_COMPILER_NAME STREQUAL "ifort")
-    target_link_libraries(check_outputs
-      PUBLIC
-      $<$<BOOL:${ENABLE_PROFILING}>:-g>         # The profiler requires both the debug and profile directives (-g and -p)
-      $<$<BOOL:${ENABLE_PROFILING}>:-p>         # The profiler requires both the debug and profile directives (-g and -p)
-      $<$<AND:$<BOOL:${LINK_TIME_OPTIMIZATION}>,$<CONFIG:RELEASE>>:-ipo>  # activate interprocediral optimization (aka link time optimization)
+      -ffree-line-length-none                   # Allow arbitrary long lines.
     )
 endif()
-
 
 # Add all test subdirectories
 add_subdirectory(arch4)

tests/comp_mods.f90
@@ -32,16 +32,16 @@ end module m_vector
 module m_stringsplitter
 contains
 ! from https://fpm.fortran-lang.org/proc/split.html
-subroutine split(input_line,array,delimiters,order,nulls)
-    !! given a line of structure " par1 par2 par3 ... parn " store each par(n) into a separate variable in array.
+subroutine split(input_line,tokens,delimiters,order,nulls)
+    !! given a line of structure " par1 par2 par3 ... parn " store each par(n) into a separate variable in tokens.
     !!
-    !! * by default adjacent delimiters in the input string do not create an empty string in the output array
+    !! * by default adjacent delimiters in the input string do not create an empty string in the output tokens
     !! * no quoting of delimiters is supported
     character(len=*),intent(in)              :: input_line  !! input string to tokenize
     character(len=*),optional,intent(in)     :: delimiters  !! list of delimiter characters
-    character(len=*),optional,intent(in)     :: order       !! order of output array sequential|[reverse|right]
+    character(len=*),optional,intent(in)     :: order       !! order of output tokens sequential|[reverse|right]
     character(len=*),optional,intent(in)     :: nulls       !! return strings composed of delimiters or not ignore|return|ignoreend
-    character(len=:),allocatable,intent(out) :: array(:)    !! output array of tokens
+    character(len=:),allocatable,intent(out) :: tokens(:)    !! output array of tokens
 
     integer                       :: n                      ! max number of strings INPUT_LINE could split into if all delimiter
     integer,allocatable           :: ibegin(:)              ! positions in input string where tokens start
@@ -59,18 +59,25 @@ subroutine split(input_line,array,delimiters,order,nulls)
     integer                       :: inotnull               ! count strings not composed of delimiters
     integer                       :: ireturn                ! number of tokens returned
     integer                       :: imax                   ! length of longest token
-
+    character, parameter :: ASCII_NULL = char(0) ! ASCII NULL character
+    character, parameter :: ASCII_SPACE = ' '
+    character, parameter :: ASCII_TAB = char(9)
+    character, parameter :: ASCII_NEWLINE = char(10)
+    character, parameter :: ASCII_VERTICAL_TAB = char(11)
+    character, parameter :: ASCII_FORM_FEED = char(12)
+    character, parameter :: ASCII_CARRIAGE_RETURN = char(13)
+    character(len=*), parameter :: default_delimiters = ASCII_SPACE//ASCII_TAB//ASCII_NEWLINE//ASCII_VERTICAL_TAB//ASCII_FORM_FEED//ASCII_CARRIAGE_RETURN//ASCII_NULL
+    allocate(character(len=0) :: dlim)
+    allocate(character(len=0) :: ordr)
+    allocate(character(len=0) :: nlls)
     ! decide on value for optional DELIMITERS parameter
+    dlim = default_delimiters   ! use default delimiter when not specified
     if (present(delimiters)) then                                     ! optional delimiter list was present
         if(delimiters.ne.'')then                                       ! if DELIMITERS was specified and not null use it
             dlim=delimiters
-        else                                                           ! DELIMITERS was specified on call as empty string
-            dlim=' '//char(9)//char(10)//char(11)//char(12)//char(13)//char(0) ! use default delimiter when not specified
         endif
-    else                                                              ! no delimiter value was specified
-        dlim=' '//char(9)//char(10)//char(11)//char(12)//char(13)//char(0)    ! use default delimiter when not specified
     endif
-    idlim=len(dlim)                                                   ! dlim a lot of blanks on some machines if dlim is a big string
+    idlim = len(dlim)                                                   ! dlim a lot of blanks on some machines if dlim is a big string
 
     if(present(order))then; ordr=(adjustl(order)); else; ordr='sequential'; endif ! decide on value for optional ORDER parameter
     if(present(nulls))then; nlls=(adjustl(nulls)); else; nlls='ignore'    ; endif ! optional parameter
@@ -123,8 +130,9 @@ subroutine split(input_line,array,delimiters,order,nulls)
     case default
         ireturn=icount
     end select
-    allocate(character(len=imax) :: array(ireturn))                ! allocate the array to return
-    !allocate(array(ireturn))                                       ! allocate the array to turn
+    if (allocated(tokens)) deallocate(tokens)  ! deallocate tokens if already allocated
+    allocate(character(len=imax) :: tokens(ireturn))                ! allocate the tokens to return
+    !allocate(tokens(ireturn))                                       ! allocate the tokens to turn
 
     select case (trim(adjustl(ordr)))                              ! decide which order to store tokens
     case ('reverse','right') ; ii=ireturn ; iiii=-1                ! last to first
@@ -136,11 +144,11 @@ subroutine split(input_line,array,delimiters,order,nulls)
             select case (trim(adjustl(nlls)))
             case ('ignore','','ignoreend')
             case default
-            array(ii)=' '
+            tokens(ii)=' '
             ii=ii+iiii
             end select
         else
-            array(ii)=input_line(ibegin(i20):iterm(i20))
+            tokens(ii)=input_line(ibegin(i20):iterm(i20))
             ii=ii+iiii
         endif
     enddo
@@ -150,12 +158,10 @@ end subroutine split
 end module m_stringsplitter
 
 function contains_time_information(line)
-    use iso_fortran_env, only: Output_Unit
     use m_stringsplitter, only: split
     ! returns true if the line contains time information (eg parts of a date, elapsed time, etc.)
     character(len=*), intent(in) :: line
     logical :: contains_time_information
-
     character(len=:), allocatable :: tokens(:)
     integer :: token_index
     call split(line, tokens)
@@ -184,7 +190,7 @@ function contains_time_information(line)
 end function contains_time_information
 
 FUNCTION is_numeric(string)
-    USE ieee_arithmetic
+    ! USE ieee_arithmetic
     IMPLICIT NONE
     CHARACTER(len=*), INTENT(IN) :: string
     LOGICAL :: is_numeric
@@ -288,7 +294,7 @@ module m_diff
     implicit none
     contains
     function vectors_differ(va, vb, tolerance, min_significant_value)
-        use iso_fortran_env, only: Error_Unit, Output_Unit
+        use iso_fortran_env, only: Error_Unit
         implicit none
         logical :: vectors_differ
         real(8), intent(in) :: va(:)
@@ -324,6 +330,7 @@ module m_diff
 
     function result_files_differ(results1_file_name, results2_file_name, num_header_lines, tolerance, min_significant_value)
         use m_vector, only: t_vector
+        use iso_fortran_env, only: Output_Unit
         ! use m_diff, only: vectors_differ
         implicit none
         character(len=200), intent(in) :: results1_file_name
@@ -332,20 +339,24 @@ module m_diff
         real(8), intent(in) :: tolerance
         real(8), intent(in) :: min_significant_value
         logical :: result_files_differ
-    
+        logical :: debug_mode
+
         type(t_vector) :: ref_numbers
         type(t_vector) :: test_numbers
-        ! integer :: number_index
-        ! real(8) :: number
+        integer :: number_index
+        real(8) :: number
     
+        debug_mode = .false.
         call get_file_numbers(results1_file_name, ref_numbers, num_header_lines(1))
-        ! number_index = 1
-        ! do while(number_index <= size(ref_numbers%array))
-        !     number = ref_numbers%array(number_index)
-        !     write(Output_Unit, *) "number(", number_index, ") = ", number
-        !     number_index = number_index + 1
-        ! enddo
-    
+        if (debug_mode) then        
+            number_index = 1
+            do while(number_index <= size(ref_numbers%array))
+                number = ref_numbers%array(number_index)
+                write(Output_Unit, *) "number(", number_index, ") = ", number
+                number_index = number_index + 1
+            enddo
+        end if
+
         call get_file_numbers(results2_file_name, test_numbers, num_header_lines(2))
     
         result_files_differ = vectors_differ(ref_numbers%array, test_numbers%array, tolerance, min_significant_value)